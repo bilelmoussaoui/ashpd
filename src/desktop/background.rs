507d3147cd49935dcee2698898fe5cd0c2c73228
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-09-10 16:51:39 +0200

    desktop: document background API

--- src/desktop/background.rs
+++ src/desktop/background.rs
@@ -1,47 +1,75 @@
+//! # Examples
+//!
+//! ```
+//! use libportal::desktop::background::{
+//!     BackgroundOptionsBuilder, BackgroundProxy, BackgroundResponse,
+//! };
+//! use libportal::{RequestProxy, WindowIdentifier};
+//!
+//! fn main() -> zbus::fdo::Result<()> {
+//!     let connection = zbus::Connection::new_session()?;
+//!     let proxy = BackgroundProxy::new(&connection)?;
+//!
+//!     let request_handle = proxy.request_background(
+//!         WindowIdentifier::default(),
+//!         BackgroundOptionsBuilder::default()
+//!             .autostart(true)
+//!             .commandline(vec!["geary".to_string()])
+//!             .reason("Automatically fetch your latest mails.")
+//!             .build(),
+//!     )?;
+//!
+//!     let request = RequestProxy::new(&connection, &request_handle)?;
+//!     request.on_response(|response: BackgroundResponse| {
+//!         if response.is_success() {
+//!             println!("{}", response.autostart());
+//!             println!("{}", response.background());
+//!         }
+//!     })?;
+//!     Ok(())
+//! }
+//! ```
 use crate::{ResponseType, WindowIdentifier};
 use serde::{Deserialize, Serialize};
 use zbus::{dbus_proxy, fdo::Result};
 use zvariant::OwnedObjectPath;
 use zvariant_derive::{DeserializeDict, SerializeDict, Type, TypeDict};
 
-#[derive(SerializeDict, DeserializeDict, TypeDict, Debug)]
+#[derive(SerializeDict, DeserializeDict, TypeDict, Debug, Default)]
 /// Specified options for a background request.
 pub struct BackgroundOptions {
     /// A string that will be used as the last element of the handle.
     pub handle_token: Option<String>,
     /// User-visible reason for the request.
-    pub reason: String,
+    pub reason: Option<String>,
     /// `true` if the app also wants to be started automatically at login.
     pub autostart: Option<bool>,
     /// if `true`, use D-Bus activation for autostart.
+    #[zvariant(rename = "dbus-activatable")]
     pub dbus_activatable: Option<bool>,
     /// Commandline to use when autostarting at login.
     /// If this is not specified, the Exec line from the desktop file will be used.
-    pub commandline: Vec<String>,
+    pub commandline: Option<Vec<String>>,
 }
 
+#[derive(Default)]
 pub struct BackgroundOptionsBuilder {
     /// A string that will be used as the last element of the handle.
-    pub handle_token: Option<String>,
+    handle_token: Option<String>,
     /// User-visible reason for the request.
-    pub reason: String,
+    reason: Option<String>,
     /// `true` if the app also wants to be started automatically at login.
-    pub autostart: Option<bool>,
+    autostart: Option<bool>,
     /// if `true`, use D-Bus activation for autostart.
-    pub dbus_activatable: Option<bool>,
+    dbus_activatable: Option<bool>,
     /// Commandline to use when autostarting at login.
-    commandline: Vec<String>,
+    commandline: Option<Vec<String>>,
 }
 
 impl BackgroundOptionsBuilder {
-    pub fn new(reason: &str) -> Self {
-        Self {
-            reason: reason.to_string(),
-            handle_token: None,
-            autostart: None,
-            dbus_activatable: None,
-            commandline: vec![],
-        }
+    pub fn reason(mut self, reason: &str) -> Self {
+        self.reason = Some(reason.to_string());
+        self
     }
 
     pub fn handle_token(mut self, handle_token: &str) -> Self {
@@ -60,7 +88,7 @@ impl BackgroundOptionsBuilder {
     }
 
     pub fn commandline(mut self, command: Vec<String>) -> Self {
-        self.commandline = command;
+        self.commandline = Some(command);
         self
     }
 
@@ -78,13 +106,27 @@ impl BackgroundOptionsBuilder {
 #[derive(Debug, Deserialize, Serialize, Type)]
 pub struct BackgroundResponse(pub ResponseType, pub BackgroundResult);
 
+impl BackgroundResponse {
+    pub fn is_success(&self) -> bool {
+        self.0 == ResponseType::Success
+    }
+
+    pub fn autostart(&self) -> bool {
+        self.1.autostart
+    }
+
+    pub fn background(&self) -> bool {
+        self.1.background
+    }
+}
+
 #[derive(SerializeDict, DeserializeDict, TypeDict, Debug)]
 /// Result returned by the response signal after a background request.
 pub struct BackgroundResult {
     /// if the application is allowed to run in the background
-    background: bool,
+    pub background: bool,
     /// if the application is will be autostarted.
-    autostart: bool,
+    pub autostart: bool,
 }
 
 #[dbus_proxy(
