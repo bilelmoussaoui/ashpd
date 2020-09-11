d345fb885686732f596f3047b869d64cab947d6a
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2020-09-11 22:26:35 +0200

    notifications: support action invoked signal

--- src/desktop/notification.rs
+++ src/desktop/notification.rs
@@ -1,9 +1,9 @@
 //! # Examples
 //!
 //! ```no_run
-//!  use libportal::desktop::notification::{
-//!     Button, Notification, NotificationProxy, Priority,
-//! };
+//! use libportal::desktop::notification::{Action, Button, Notification, NotificationProxy, Priority};
+//! use libportal::zbus;
+//! use libportal::zvariant::Value;
 //! use std::{thread, time};
 //!
 //! fn main() -> zbus::fdo::Result<()> {
@@ -13,22 +13,39 @@
 //!     let notification_id = "org.gnome.design.Contrast";
 //!     proxy.add_notification(
 //!         notification_id,
-//!         Notification::new("Contrast", "close")
+//!         Notification::new("Contrast")
+//!             .default_action("open")
+//!             .default_action_target(Value::U32(100).into())
 //!             .body("color copied to clipboard")
 //!             .priority(Priority::High)
-//!             .button(Button::new("Copy", "copy")),
+//!             .button(Button::new("Copy", "copy").target(Value::U32(32).into()))
+//!             .button(Button::new("Delete", "delete").target(Value::U32(40).into())),
 //!     )?;
+//!
+//!     proxy.on_action_invoked(|action: Action| {
+//!         match action.name() {
+//!             "copy" => (),   // Copy something to clipboard
+//!             "delete" => (), // Delete the file
+//!             _ => (),
+//!         };
+//!         println!("{:#?}", action.id());
+//!         println!(
+//!             "{:#?}",
+//!             action.parameter().get(0).unwrap().downcast_ref::<u32>()
+//!         )
+//!     })?;
 //!
 //!     thread::sleep(time::Duration::from_secs(1));
 //!     proxy.remove_notification(notification_id)?;
 //!     Ok(())
 //! }
+//!
 //!```
 use serde::{self, Deserialize, Serialize, Serializer};
 use strum_macros::{AsRefStr, EnumString, IntoStaticStr, ToString};
-use zbus::{dbus_proxy, fdo::Result};
+use zbus::{fdo::Result, Connection, Proxy};
 use zvariant::{OwnedValue, Signature};
-use zvariant_derive::{DeserializeDict, SerializeDict, TypeDict};
+use zvariant_derive::{DeserializeDict, SerializeDict, Type, TypeDict};
 
 #[derive(
     Debug, Clone, Deserialize, AsRefStr, EnumString, IntoStaticStr, ToString, PartialEq, Eq,
@@ -69,24 +86,24 @@ pub struct Notification {
     pub priority: Option<Priority>,
     /// Name of an action that is exported by the application. This action will be activated when the user clicks on the notification.
     #[zvariant(rename = "default-action")]
-    pub default_action: String,
+    pub default_action: Option<String>,
     /// Target parameter to send along when activating the default action.
     #[zvariant(rename = "default-action-target")]
     pub default_action_target: Option<OwnedValue>,
     /// Array of buttons to add to the notification.
-    pub buttons: Vec<Button>,
+    pub buttons: Option<Vec<Button>>,
 }
 
 impl Notification {
-    pub fn new(title: &str, default_action: &str) -> Self {
+    pub fn new(title: &str) -> Self {
         Self {
             title: title.to_string(),
             body: None,
             priority: None,
             icon: None,
-            default_action: default_action.to_string(),
+            default_action: None,
             default_action_target: None,
-            buttons: vec![],
+            buttons: None,
         }
     }
 
@@ -105,13 +122,23 @@ impl Notification {
         self
     }
 
+    pub fn default_action(mut self, default_action: &str) -> Self {
+        self.default_action = Some(default_action.to_string());
+        self
+    }
+
     pub fn default_action_target(mut self, default_action_target: OwnedValue) -> Self {
         self.default_action_target = Some(default_action_target);
         self
     }
 
     pub fn button(mut self, button: Button) -> Self {
-        self.buttons.push(button);
+        match self.buttons {
+            Some(ref mut buttons) => buttons.push(button),
+            None => {
+                self.buttons.replace(vec![button]);
+            }
+        };
         self
     }
 }
@@ -142,11 +169,26 @@ impl Button {
     }
 }
 
-#[dbus_proxy(
-    interface = "org.freedesktop.portal.Notification",
-    default_service = "org.freedesktop.portal.Desktop",
-    default_path = "/org/freedesktop/portal/desktop"
-)]
+#[derive(Debug, Serialize, Deserialize, Type)]
+pub struct Action<'a>(&'a str, &'a str, Vec<OwnedValue>);
+
+impl<'a> Action<'a> {
+    /// Notification ID.
+    pub fn id(&self) -> &'a str {
+        self.0
+    }
+
+    /// Action name.
+    pub fn name(&self) -> &'a str {
+        self.1
+    }
+
+    /// The parameters passed to the action.
+    pub fn parameter(&self) -> Vec<OwnedValue> {
+        self.2.clone()
+    }
+}
+
 /// The interface lets sandboxed applications send and withdraw notifications.
 ///
 /// It is not possible for the application to learn if the notification was actually
@@ -162,7 +204,41 @@ impl Button {
 /// Other actions are activated by sending the
 ///  #org.freedeskop.portal.Notification::ActionInvoked signal to the application.
 ///
-trait Notification {
+pub struct NotificationProxy<'a> {
+    proxy: Proxy<'a>,
+    connection: &'a Connection,
+}
+
+impl<'a> NotificationProxy<'a> {
+    pub fn new(connection: &'a Connection) -> Result<Self> {
+        let proxy = Proxy::new(
+            connection,
+            "org.freedesktop.portal.Desktop",
+            "/org/freedesktop/portal/desktop",
+            "org.freedesktop.portal.Notification",
+        )?;
+        Ok(Self { proxy, connection })
+    }
+
+    /// Signal emitted when a particular action is invoked
+    pub fn on_action_invoked<F>(&self, callback: F) -> Result<()>
+    where
+        F: FnOnce(Action),
+    {
+        loop {
+            let msg = self.connection.receive_message()?;
+            let msg_header = msg.header()?;
+            if msg_header.message_type()? == zbus::MessageType::Signal
+                && msg_header.member()? == Some("ActionInvoked")
+            {
+                let response = msg.body::<Action>()?;
+                callback(response);
+                break;
+            }
+        }
+        Ok(())
+    }
+
     /// Sends a notification.
     ///
     /// The ID can be used to later withdraw the notification.
@@ -172,19 +248,23 @@ trait Notification {
     ///
     /// * `id` - Application-provided ID for this notification
     /// * `notification` - HashMap
-    fn add_notification(&self, id: &str, notification: Notification) -> Result<()>;
+    pub fn add_notification(&self, id: &str, notification: Notification) -> Result<()> {
+        self.proxy.call("AddNotification", &(id, notification))?;
+        Ok(())
+    }
 
     /// Withdraws a notification.
     ///
     /// # Arguments
     ///
     /// * `id` - Application-provided ID for this notification
-    fn remove_notification(&self, id: &str) -> Result<()>;
-
-    // FIXME: enable once signals are in
-    // fn action_invoked(&self, id: &str, action: &str, params: &[Value]);
+    pub fn remove_notification(&self, id: &str) -> Result<()> {
+        self.proxy.call("RemoveNotification", &(id))?;
+        Ok(())
+    }
 
     /// version property
-    #[dbus_proxy(property, name = "version")]
-    fn version(&self) -> Result<u32>;
+    pub fn version(&self) -> Result<u32> {
+        self.proxy.get_property::<u32>("version")
+    }
 }
