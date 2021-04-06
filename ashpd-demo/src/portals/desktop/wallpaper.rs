36c0b6fc2d4637b5468845c14564ef2f89e3038b
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-04-06 19:48:41 +0200

    demo: make the wallpaper page working

--- ashpd-demo/src/portals/desktop/wallpaper.rs
+++ ashpd-demo/src/portals/desktop/wallpaper.rs
@@ -1,5 +1,10 @@
+use std::{cell::RefCell, str::FromStr, sync::Arc};
+
+use adw::prelude::*;
+use ashpd::desktop::wallpaper::{AsyncWallpaperProxy, SetOn, WallpaperOptions};
 use ashpd::zbus;
-use ashpd::{RequestProxy, Response, WindowIdentifier};
+use ashpd::{BasicResponse, Response, WindowIdentifier};
+use futures::{lock::Mutex, FutureExt};
 use gtk::glib;
 use gtk::prelude::*;
 use gtk::subclass::prelude::*;
@@ -12,7 +17,13 @@ mod imp {
 
     #[derive(Debug, CompositeTemplate, Default)]
     #[template(resource = "/com/belmoussaoui/ashpd/demo/wallpaper.ui")]
-    pub struct WallpaperPage {}
+    pub struct WallpaperPage {
+        pub dialog: RefCell<Option<gtk::FileChooserNative>>,
+        #[template_child]
+        pub preview_switch: TemplateChild<gtk::Switch>,
+        #[template_child]
+        pub set_on_combo: TemplateChild<adw::ComboRow>,
+    }
 
     #[glib::object_subclass]
     impl ObjectSubclass for WallpaperPage {
@@ -23,7 +34,7 @@ mod imp {
         fn class_init(klass: &mut Self::Class) {
             Self::bind_template(klass);
             klass.install_action("wallpaper.select", None, move |page, _action, _target| {
-                //page.pick_color().unwrap();
+                page.pick_wallpaper();
             });
         }
 
@@ -31,7 +42,12 @@ mod imp {
             obj.init_template();
         }
     }
-    impl ObjectImpl for WallpaperPage {}
+    impl ObjectImpl for WallpaperPage {
+        fn constructed(&self, _obj: &Self::Type) {
+            let model = gtk::StringList::new(&["Background", "Lockscreen", "Both"]);
+            self.set_on_combo.set_model(Some(&model));
+        }
+    }
     impl WidgetImpl for WallpaperPage {}
     impl BinImpl for WallpaperPage {}
 }
@@ -44,4 +60,91 @@ impl WallpaperPage {
     pub fn new() -> Self {
         glib::Object::new(&[]).expect("Failed to create a WallpaperPage")
     }
+
+    pub fn pick_wallpaper(&self) {
+        let self_ = imp::WallpaperPage::from_instance(self);
+        let file_chooser = gtk::FileChooserNativeBuilder::new()
+            .accept_label("Select")
+            .action(gtk::FileChooserAction::Open)
+            .modal(true)
+            .build();
+        let filter = gtk::FileFilter::new();
+        filter.add_pixbuf_formats();
+        filter.set_name(Some("images"));
+        file_chooser.add_filter(&filter);
+
+        let show_preview = self_.preview_switch.get().get_active();
+        let selected_item = self_.set_on_combo.get_selected_item().unwrap();
+        let set_on = SetOn::from_str(
+            &selected_item
+                .downcast_ref::<gtk::StringObject>()
+                .unwrap()
+                .get_string(),
+        )
+        .unwrap();
+
+        file_chooser.connect_response(move |dialog, response| {
+            if response == gtk::ResponseType::Accept {
+                let wallpaper_uri = dialog.get_file().unwrap().get_uri();
+                let ctx = glib::MainContext::default();
+                ctx.spawn_local(async move {
+                    if let Ok(Response::Ok(color)) = set_wallpaper_uri(
+                        WindowIdentifier::default(),
+                        &wallpaper_uri,
+                        show_preview,
+                        set_on,
+                    )
+                    .await
+                    {
+                        // TODO: display a notification saying the action went fine
+                        println!("{:#?}", color);
+                    }
+                });
+            };
+            dialog.destroy();
+        });
+        file_chooser.show();
+        self_.dialog.replace(Some(file_chooser));
+    }
+}
+
+pub async fn set_wallpaper_uri(
+    window_identifier: WindowIdentifier,
+    wallpaper_uri: &str,
+    show_preview: bool,
+    set_on: SetOn,
+) -> zbus::Result<Response<BasicResponse>> {
+    let connection = zbus::azync::Connection::new_session().await?;
+    let proxy = AsyncWallpaperProxy::new(&connection)?;
+    let request = proxy
+        .set_wallpaper_uri(
+            window_identifier,
+            wallpaper_uri,
+            WallpaperOptions::default()
+                .show_preview(show_preview)
+                .set_on(set_on),
+        )
+        .await?;
+
+    let (sender, receiver) = futures::channel::oneshot::channel();
+
+    let sender = Arc::new(Mutex::new(Some(sender)));
+    let signal_id = request
+        .connect_response(move |response: Response<BasicResponse>| {
+            let s = sender.clone();
+            async move {
+                if let Some(m) = s.lock().await.take() {
+                    let _ = m.send(response);
+                }
+                Ok(())
+            }
+            .boxed()
+        })
+        .await?;
+
+    while request.next_signal().await?.is_some() {}
+    request.disconnect_signal(signal_id).await?;
+
+    let wallpaper = receiver.await.unwrap();
+    Ok(wallpaper)
 }
