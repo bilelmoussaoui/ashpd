f4acdae21c48c23ec323a5bb4976445fa64b0ec6
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-04-06 15:02:02 +0200

    demo: make the screenshot/account pages working

---
+++ ashpd-demo/src/portals/desktop/background.rs
@@ -0,0 +1,54 @@
+use ashpd::desktop::network_monitor::NetworkMonitorProxy;
+use ashpd::zbus;
+use ashpd::{RequestProxy, Response, WindowIdentifier};
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+
+mod imp {
+    use adw::subclass::prelude::*;
+    use gtk::CompositeTemplate;
+
+    use super::*;
+
+    #[derive(Debug, CompositeTemplate)]
+    #[template(resource = "/com/belmoussaoui/ashpd/demo/background.ui")]
+    pub struct BackgroundPage {}
+
+    impl Default for BackgroundPage {
+        fn default() -> Self {
+            Self {}
+        }
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for BackgroundPage {
+        const NAME: &'static str = "BackgroundPage";
+        type Type = super::BackgroundPage;
+        type ParentType = adw::Bin;
+
+        fn class_init(klass: &mut Self::Class) {
+            Self::bind_template(klass);
+            klass.install_action("background.request", None, move |page, _action, _target| {
+                //page.pick_color().unwrap();
+            });
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+    impl ObjectImpl for BackgroundPage {}
+    impl WidgetImpl for BackgroundPage {}
+    impl BinImpl for BackgroundPage {}
+}
+
+glib::wrapper! {
+    pub struct BackgroundPage(ObjectSubclass<imp::BackgroundPage>) @extends gtk::Widget, adw::Bin;
+}
+
+impl BackgroundPage {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).expect("Failed to create a BackgroundPage")
+    }
+}
---
+++ ashpd-demo/src/portals/desktop/email.rs
@@ -0,0 +1,54 @@
+use ashpd::desktop::network_monitor::NetworkMonitorProxy;
+use ashpd::zbus;
+use ashpd::{RequestProxy, Response, WindowIdentifier};
+use gtk::glib;
+use gtk::prelude::*;
+use gtk::subclass::prelude::*;
+
+mod imp {
+    use adw::subclass::prelude::*;
+    use gtk::CompositeTemplate;
+
+    use super::*;
+
+    #[derive(Debug, CompositeTemplate)]
+    #[template(resource = "/com/belmoussaoui/ashpd/demo/email.ui")]
+    pub struct EmailPage {}
+
+    impl Default for EmailPage {
+        fn default() -> Self {
+            Self {}
+        }
+    }
+
+    #[glib::object_subclass]
+    impl ObjectSubclass for EmailPage {
+        const NAME: &'static str = "EmailPage";
+        type Type = super::EmailPage;
+        type ParentType = adw::Bin;
+
+        fn class_init(klass: &mut Self::Class) {
+            Self::bind_template(klass);
+            klass.install_action("background.request", None, move |page, _action, _target| {
+                //page.pick_color().unwrap();
+            });
+        }
+
+        fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
+            obj.init_template();
+        }
+    }
+    impl ObjectImpl for EmailPage {}
+    impl WidgetImpl for EmailPage {}
+    impl BinImpl for EmailPage {}
+}
+
+glib::wrapper! {
+    pub struct EmailPage(ObjectSubclass<imp::EmailPage>) @extends gtk::Widget, adw::Bin;
+}
+
+impl EmailPage {
+    pub fn new() -> Self {
+        glib::Object::new(&[]).expect("Failed to create a EmailPage")
+    }
+}
