8af3c7630e3e1cd4fb0e1b466a289f54ac89b54c
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-04-10 01:28:54 +0200

    trash: add a helper function and use it

--- src/desktop/trash.rs
+++ src/desktop/trash.rs
@@ -1,35 +1,32 @@
 //! # Examples
 //!
 //! ```rust,no_run
-//! use ashpd::desktop::trash::{TrashProxy, TrashStatus};
 //! use std::fs::File;
 //! use std::os::unix::io::AsRawFd;
+//! use ashpd::desktop::trash::{trash_file, TrashStatus};
 //! use zbus::fdo::Result;
-//! use zvariant::Fd;
 //!
-//! fn main() -> Result<()> {
-//!     let connection = zbus::Connection::new_session()?;
-//!     let proxy = TrashProxy::new(&connection)?;
-//!
+//! async fn run() -> Result<()> {
 //!     let file = File::open("/home/bilelmoussaoui/Downloads/adwaita-night.jpg").unwrap();
 //!
-//!     match proxy.trash_file(Fd::from(file.as_raw_fd()))? {
+//!     match trash_file(file.as_raw_fd()).await? {
 //!         TrashStatus::Succeeded => println!("the file was removed!"),
 //!         TrashStatus::Failed => println!("oof, couldn't remove the file"),
-//!         _ => println!("something else happened"),
 //!     };
 //!
 //!     Ok(())
 //! }
 //! ```
+use std::os::unix::io::AsRawFd;
+
+use serde::Serialize;
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use zbus::{dbus_proxy, fdo::Result};
-use zvariant::Fd;
+use zvariant::Type;
 use zvariant_derive::Type;
 
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Clone, Copy, Hash, Debug, Type)]
 #[repr(u32)]
-#[non_exhaustive]
 /// The status of moving a file to the trash.
 pub enum TrashStatus {
     /// Moving the file to the trash failed.
@@ -52,9 +49,18 @@ trait Trash {
     /// # Arguments
     ///
     /// * `fd` - The file descriptor.
-    fn trash_file(&self, fd: Fd) -> Result<TrashStatus>;
+    fn trash_file<F: AsRawFd + Type + Serialize>(&self, fd: F) -> Result<TrashStatus>;
 
     /// The version of this DBus interface.
     #[dbus_proxy(property, name = "version")]
     fn version(&self) -> Result<u32>;
+}
+
+/// Sends a file to the trash
+///
+/// A helper function around the `AsyncTrashProxy::trash_file`.
+pub async fn trash_file<F: AsRawFd + Type + Serialize>(file: F) -> Result<TrashStatus> {
+    let connection = zbus::azync::Connection::new_session().await?;
+    let proxy = AsyncTrashProxy::new(&connection)?;
+    proxy.trash_file(file).await
 }
