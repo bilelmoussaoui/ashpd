d13acf17fa6a70d95e1310170321dce2ddafddf2
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-07-29 00:59:53 +0200

    demo: update build-aux scripts

--- ashpd-demo/build-aux/cargo.sh
+++ ashpd-demo/build-aux/cargo.sh
@@ -3,18 +3,16 @@
 export MESON_BUILD_ROOT="$1"
 export MESON_SOURCE_ROOT="$2"
 export CARGO_TARGET_DIR="$MESON_BUILD_ROOT"/target
-export CARGO_HOME="$CARGO_TARGET_DIR"/cargo-home
+export CARGO_HOME="$MESON_BUILD_ROOT"/cargo-home
 
-if [[ $4 = "Devel" ]]
-then
+if [[ $4 = "Devel" ]]; then
     echo "DEBUG MODE"
     cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml && \
+        "$MESON_SOURCE_ROOT"/Cargo.toml &&
         cp "$CARGO_TARGET_DIR"/debug/$5 $3
 else
     echo "RELEASE MODE"
     cargo build --manifest-path \
-        "$MESON_SOURCE_ROOT"/Cargo.toml --release && \
+        "$MESON_SOURCE_ROOT"/Cargo.toml --release &&
         cp "$CARGO_TARGET_DIR"/release/$5 $3
 fi
-
--- ashpd-demo/build-aux/dist-vendor.sh
+++ ashpd-demo/build-aux/dist-vendor.sh
@@ -4,7 +4,6 @@ export SOURCE_ROOT="$2"
 
 cd "$SOURCE_ROOT"
 mkdir "$DIST"/.cargo
-cargo vendor | sed 's/^directory = ".*"/directory = "vendor"/g' > $DIST/.cargo/config
+cargo vendor | sed 's/^directory = ".*"/directory = "vendor"/g' >$DIST/.cargo/config
 # Move vendor into dist tarball directory
 mv vendor "$DIST"
-
