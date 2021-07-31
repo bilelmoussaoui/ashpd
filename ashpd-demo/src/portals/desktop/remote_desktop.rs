81759119e90e89c93f946ab0ddbf585385502882
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2021-07-31 15:25:51 +0200

    demo: finish the Remote desktop page

--- ashpd-demo/src/portals/desktop/remote_desktop.rs
+++ ashpd-demo/src/portals/desktop/remote_desktop.rs
@@ -22,7 +22,7 @@ mod imp {
 
     use super::*;
 
-    #[derive(Debug, CompositeTemplate)]
+    #[derive(Debug, Default, CompositeTemplate)]
     #[template(resource = "/com/belmoussaoui/ashpd/demo/remote_desktop.ui")]
     pub struct RemoteDesktopPage {
         #[template_child]
@@ -30,16 +30,26 @@ mod imp {
         pub session: Arc<Mutex<Option<SessionProxy<'static>>>>,
         #[template_child]
         pub close_session_btn: TemplateChild<gtk::Button>,
-    }
-
-    impl Default for RemoteDesktopPage {
-        fn default() -> Self {
-            Self {
-                response_group: TemplateChild::default(),
-                close_session_btn: TemplateChild::default(),
-                session: Arc::new(Mutex::new(None)),
-            }
-        }
+        #[template_child]
+        pub screencast_row: TemplateChild<adw::ExpanderRow>,
+        #[template_child]
+        pub multiple_switch: TemplateChild<gtk::Switch>,
+        #[template_child]
+        pub keyboard_check: TemplateChild<gtk::CheckButton>,
+        #[template_child]
+        pub pointer_check: TemplateChild<gtk::CheckButton>,
+        #[template_child]
+        pub touchscreen_check: TemplateChild<gtk::CheckButton>,
+        #[template_child]
+        pub monitor_check: TemplateChild<gtk::CheckButton>,
+        #[template_child]
+        pub window_check: TemplateChild<gtk::CheckButton>,
+        #[template_child]
+        pub hidden_check: TemplateChild<gtk::CheckButton>,
+        #[template_child]
+        pub embedded_check: TemplateChild<gtk::CheckButton>,
+        #[template_child]
+        pub metadata_check: TemplateChild<gtk::CheckButton>
     }
 
     #[glib::object_subclass]
@@ -92,6 +102,53 @@ impl RemoteDesktopPage {
         glib::Object::new(&[]).expect("Failed to create a RemoteDesktopPage")
     }
 
+    /// Returns the selected DeviceType
+    fn selected_devices(&self) -> BitFlags<DeviceType> {
+        let self_ = imp::RemoteDesktopPage::from_instance(self);
+
+        let mut devices: BitFlags<DeviceType> = BitFlags::empty();
+        if self_.keyboard_check.is_active() {
+            devices.insert(DeviceType::Keyboard);
+        }
+        if self_.pointer_check.is_active() {
+            devices.insert(DeviceType::Pointer);
+        }
+        if self_.touchscreen_check.is_active() {
+            devices.insert(DeviceType::Touchscreen);
+        }
+        devices
+    }
+
+    /// Returns the selected SourceType
+    fn selected_sources(&self) -> BitFlags<SourceType> {
+        let self_ = imp::RemoteDesktopPage::from_instance(self);
+        let mut sources: BitFlags<SourceType> = BitFlags::empty();
+        if self_.monitor_check.is_active() {
+            sources.insert(SourceType::Monitor);
+        }
+        if self_.window_check.is_active() {
+            sources.insert(SourceType::Window);
+        }
+        sources
+    }
+
+    /// Returns the selected CursorMode
+    fn selected_cursor_mode(&self) -> BitFlags<CursorMode> {
+        let self_ = imp::RemoteDesktopPage::from_instance(self);
+
+        let mut cursor_mode: BitFlags<CursorMode> = BitFlags::empty();
+        if self_.hidden_check.is_active() {
+            cursor_mode.insert(CursorMode::Hidden);
+        }
+        if self_.embedded_check.is_active() {
+            cursor_mode.insert(CursorMode::Embedded);
+        }
+        if self_.metadata_check.is_active() {
+            cursor_mode.insert(CursorMode::Metadata);
+        }
+        cursor_mode
+    }
+
     pub fn start_session(&self) {
         let ctx = glib::MainContext::default();
         ctx.spawn_local(clone!(@weak self as page => async move {
@@ -100,11 +157,15 @@ impl RemoteDesktopPage {
             let root = page.native().unwrap();
             page.action_set_enabled("remote_desktop.start", false);
             page.action_set_enabled("remote_desktop.stop", true);
+            let is_screencast = self_.screencast_row.get().enables_expansion();
+            let multiple_sources = self_.multiple_switch.is_active();
+            let cursor_mode = page.selected_cursor_mode();
+            let sources = page.selected_sources();
+            let devices = page.selected_devices();
 
             let identifier = WindowIdentifier::from_native(&root).await;
-
-            match remote(&identifier,DeviceType::Keyboard | DeviceType::Pointer).await {
-                Ok((devices, streams, session)) => {
+            match remote(&identifier, devices, is_screencast, multiple_sources, cursor_mode, sources).await {
+                Ok((selected_devices, streams, session)) => {
                     self_.response_group.show();
                     self_.session.lock().await.replace(session);
                     self_.close_session_btn.set_sensitive(true);
@@ -135,22 +196,21 @@ impl RemoteDesktopPage {
 pub async fn remote(
     identifier: &WindowIdentifier,
     devices: BitFlags<DeviceType>,
+    is_screencast: bool,
+    multiple_sources: bool,
+    cursor_mode: BitFlags<CursorMode>,
+    sources: BitFlags<SourceType>,
 ) -> ashpd::Result<(BitFlags<DeviceType>, Vec<Stream>, SessionProxy<'static>)> {
     let connection = zbus::azync::Connection::session().await?;
     let proxy = RemoteDesktopProxy::new(&connection).await?;
-    let screencast = ScreenCastProxy::new(&connection).await?;
-
     let session = proxy.create_session().await?;
+    if is_screencast {
+        let screencast_proxy = ScreenCastProxy::new(&connection).await?;
+        screencast_proxy
+            .select_sources(&session, cursor_mode, sources, multiple_sources)
+            .await?;
+    }
     proxy.select_devices(&session, devices).await?;
-
-    screencast
-        .select_sources(
-            &session,
-            CursorMode::Metadata.into(),
-            SourceType::Monitor | SourceType::Window,
-            true,
-        )
-        .await?;
 
     let (devices, streams) = proxy.start(&session, identifier).await?;
     Ok((devices, streams, session))
