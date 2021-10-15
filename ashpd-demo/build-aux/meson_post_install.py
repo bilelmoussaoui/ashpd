31bbf9bb65d5a6a53f9bc8da656166eee3a2eb42
Jan Tojnar <jtojnar@gmail.com>
2021-10-15 17:22:45 +0200

    build: use gtk4-update-icon-cache

--- ashpd-demo/build-aux/meson_post_install.py
+++ ashpd-demo/build-aux/meson_post_install.py
@@ -7,7 +7,7 @@ if not environ.get('DESTDIR', ''):
     PREFIX = environ.get('MESON_INSTALL_PREFIX', '/usr/local')
     DATA_DIR = path.join(PREFIX, 'share')
     print('Updating icon cache...')
-    call(['gtk-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
+    call(['gtk4-update-icon-cache', '-qtf', path.join(DATA_DIR, 'icons/hicolor')])
     print("Compiling new schemas...")
     call(["glib-compile-schemas", path.join(DATA_DIR, 'glib-2.0/schemas')])
     print("Updating desktop database...")
