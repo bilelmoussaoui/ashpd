9c5e5360aa706985f035ecb09c892216d8edc360
Bilal Elmoussaoui <bil.elmoussaoui@gmail.com>
2022-01-04 21:04:51 +0100

    bump to zbus 2.0.0

--- src/desktop/handle_token.rs
+++ src/desktop/handle_token.rs
@@ -4,7 +4,7 @@ use std::{convert::TryFrom, fmt::Display};
 use rand::distributions::Alphanumeric;
 use rand::{thread_rng, Rng};
 use serde::{Deserialize, Serialize};
-use zbus_names::OwnedMemberName;
+use zbus::names::OwnedMemberName;
 use zvariant_derive::Type;
 
 /// A handle token is a DBus Object Path element, specified in the
