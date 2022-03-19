418645d06bb6fab6434a36a6642c0a2ab5d84f4a
Bilal Elmoussaoui <belmouss@redhat.com>
2022-03-19 15:32:42 +0100

    window identifier: Rename WindowType to WindowIdentifierType
    The idea is to avoid duplicating the x11/wayland way of passing the handle
    to the portal.

--- src/window_identifier/x11.rs
+++ src/window_identifier/x11.rs
@@ -1,19 +1,16 @@
 use std::fmt;
 
-pub struct X11WindowIdentifier(u64);
+use super::WindowIdentifierType;
+pub struct X11WindowIdentifier(WindowIdentifierType);
 
 impl X11WindowIdentifier {
     pub fn new(xid: u64) -> Self {
-        Self(xid)
+        Self(WindowIdentifierType::X11(xid))
     }
 }
 
 impl fmt::Display for X11WindowIdentifier {
     fn fmt(&self, f: &mut fmt::Formatter<'_>) -> fmt::Result {
-        f.write_str(&to_handle(self.0))
+        f.write_str(&format!("{}", self.0))
     }
-}
-
-pub fn to_handle(xid: u64) -> String {
-    format!("x11:0x{:x}", xid)
 }
