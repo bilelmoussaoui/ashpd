6a31d7330a6a7daed5532c4e61ee675ec38f93e2
Bilal Elmoussaoui <belmouss@redhat.com>
2022-06-08 23:48:08 +0200

    stop exporting OnceCell

--- src/lib.rs
+++ src/lib.rs
@@ -10,7 +10,7 @@ compile_error!("You can't enable both GTK 3 & GTK 4 features at once");
 #[cfg(all(all(feature = "tokio", feature = "async-std"), not(doc)))]
 compile_error!("You can't enable both async-std & tokio features at once");
 
-pub use once_cell::sync::OnceCell;
+use once_cell::sync::OnceCell;
 
 static SESSION: OnceCell<zbus::Connection> = OnceCell::new();
 
