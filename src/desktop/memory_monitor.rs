3f77bb92babe8d2f3d1347d5e8617c17cbb0dccd
Bilal Elmoussaoui <belmouss@redhat.com>
2022-06-11 20:42:37 +0200

    desktop/memory_monitor: Drop Proxy suffix

--- src/desktop/memory_monitor.rs
+++ src/desktop/memory_monitor.rs
@@ -1,14 +1,12 @@
 //! # Examples
 //!
 //! ```rust,no_run
-//! use ashpd::desktop::memory_monitor::MemoryMonitorProxy;
+//! use ashpd::desktop::memory_monitor::MemoryMonitor;
 //!
 //! async fn run() -> ashpd::Result<()> {
-//!     let proxy = MemoryMonitorProxy::new().await?;
-//!
+//!     let proxy = MemoryMonitor::new().await?;
 //!     let level = proxy.receive_low_memory_warning().await?;
 //!     println!("{}", level);
-//!
 //!     Ok(())
 //! }
 //! ```
@@ -20,17 +18,19 @@ use crate::{
 };
 
 /// The interface provides information about low system memory to sandboxed
-/// applications. It is not a portal in the strict sense, since it does not
-/// involve user interaction.
+/// applications.
+///
+/// It is not a portal in the strict sense, since it does not involve user
+/// interaction.
 ///
 /// Wrapper of the DBus interface: [`org.freedesktop.portal.MemoryMonitor`](https://flatpak.github.io/xdg-desktop-portal/index.html#gdbus-org.freedesktop.portal.MemoryMonitor).
 #[derive(Debug)]
 #[doc(alias = "org.freedesktop.portal.MemoryMonitor")]
-pub struct MemoryMonitorProxy<'a>(zbus::Proxy<'a>);
+pub struct MemoryMonitor<'a>(zbus::Proxy<'a>);
 
-impl<'a> MemoryMonitorProxy<'a> {
-    /// Create a new instance of [`MemoryMonitorProxy`].
-    pub async fn new() -> Result<MemoryMonitorProxy<'a>, Error> {
+impl<'a> MemoryMonitor<'a> {
+    /// Create a new instance of [`MemoryMonitor`].
+    pub async fn new() -> Result<MemoryMonitor<'a>, Error> {
         let connection = session_connection().await?;
         let proxy = zbus::ProxyBuilder::new_bare(&connection)
             .interface("org.freedesktop.portal.MemoryMonitor")?
