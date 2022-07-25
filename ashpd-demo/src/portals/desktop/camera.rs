d2bea118c86318b8bed733a5436bcdb29f483715
Maximiliano Sandoval R <msandova@gnome.org>
2022-07-25 11:05:19 +0200

    demo: Allow selecting camera

--- ashpd-demo/src/portals/desktop/camera.rs
+++ ashpd-demo/src/portals/desktop/camera.rs
@@ -10,6 +10,7 @@ use crate::widgets::{
 
 mod imp {
     use adw::subclass::prelude::*;
+    use std::cell::RefCell;
 
     use super::*;
 
@@ -19,10 +20,13 @@ mod imp {
         #[template_child]
         pub camera_available: TemplateChild<gtk::Label>,
         #[template_child]
-        pub picture: TemplateChild<gtk::Picture>,
-        pub paintable: CameraPaintable,
+        pub picture_stack: TemplateChild<gtk::Stack>,
+        #[template_child]
+        pub picture_stack_switcher: TemplateChild<gtk::StackSwitcher>,
         #[template_child]
         pub revealer: TemplateChild<gtk::Revealer>,
+
+        pub paintables: RefCell<Vec<CameraPaintable>>,
     }
 
     #[glib::object_subclass]
@@ -51,7 +55,6 @@ mod imp {
     }
     impl ObjectImpl for CameraPage {
         fn constructed(&self, obj: &Self::Type) {
-            self.picture.set_paintable(Some(&self.paintable));
             obj.action_set_enabled("camera.stop", false);
             self.parent_constructed(obj);
         }
@@ -96,10 +99,40 @@ impl CameraPage {
         self.action_set_enabled("camera.start", false);
         match stream().await {
             Ok(stream_fd) => {
-                let node_id = camera::pipewire_node_id(stream_fd).await.unwrap();
-                imp.paintable.set_pipewire_node_id(stream_fd, node_id);
-                imp.revealer.set_reveal_child(true);
-                imp.camera_available.set_text("Yes");
+                let streams = camera::pipewire_streams(stream_fd).await.unwrap();
+                let n_cameras = streams.len();
+                for s in streams.iter() {
+                    let picture = gtk::Picture::new();
+                    let paintable = CameraPaintable::new();
+                    let props = s.properties();
+                    let nick = props
+                        .get("node.nick")
+                        .or(props.get("node.description"))
+                        .or(props.get("node.name"))
+                        .map(String::from)
+                        .unwrap_or_default();
+                    let name = props.get("name").map(String::from).unwrap_or_default();
+                    tracing::debug!(
+                        "Found camera. node_id: {}, nick: {nick}, name: {name}",
+                        s.node_id()
+                    );
+
+                    picture.set_vexpand(true);
+                    picture.set_paintable(Some(&paintable));
+                    paintable.set_pipewire_node_id(stream_fd, Some(s.node_id()));
+
+                    self.imp()
+                        .picture_stack
+                        .add_titled(&picture, Some(&name), &nick);
+                    self.imp().paintables.borrow_mut().push(paintable.clone());
+                }
+                imp.revealer.set_reveal_child(n_cameras > 0);
+                imp.picture_stack_switcher.set_visible(n_cameras > 1);
+                if n_cameras > 0 {
+                    imp.camera_available.set_text("Yes");
+                } else {
+                    imp.camera_available.set_text("No");
+                }
 
                 self.send_notification(
                     "Camera stream started successfully",
@@ -122,7 +155,13 @@ impl CameraPage {
         self.action_set_enabled("camera.stop", false);
         self.action_set_enabled("camera.start", true);
 
-        imp.paintable.close_pipeline();
+        for paintable in imp.paintables.take() {
+            paintable.close_pipeline();
+        }
+        while let Some(child) = imp.picture_stack.first_child() {
+            imp.picture_stack.remove(&child);
+        }
+
         imp.revealer.set_reveal_child(false);
     }
 }
