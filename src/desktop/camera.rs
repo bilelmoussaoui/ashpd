e8d1b93517fede128c4b6f7a218b39dd10974d43
Syed Ahkam <smahkam57@gmail.com>
2024-11-19 00:57:14 +0530

    docs: prevent pw doctest from compiling

--- src/desktop/camera.rs
+++ src/desktop/camera.rs
@@ -18,7 +18,7 @@
 //! ```
 //! An example on how to connect with Pipewire can be found [here](https://github.com/bilelmoussaoui/ashpd/blob/master/examples/screen_cast_pw.rs).
 //! Although the example's primary focus is screen casting, stream connection logic remains the same -- with one accessibility change:
-//! ```rust,no_run
+//! ```rust,ignore
 //! let stream = pw::stream::Stream::new(
 //!    &core,
 //!    "video-test",
