419693522a32f48878864b689dffce70038dee6b
Olivier Tilloy <otilloy@igalia.com>
2025-03-10 18:27:20 +0100

    backend-demo: Implement the Access interface
    Without it, the xdg-desktop-portal frontend will skip looking for an
    implementation of the Screenshot and Wallpaper interfaces, among others
    (see https://github.com/flatpak/xdg-desktop-portal/blob/2fb76ffb/src/xdg-desktop-portal.c#L321-L358).

---
+++ backend-demo/src/access.rs
@@ -0,0 +1,40 @@
+use ashpd::{
+    backend::{
+        access::{AccessImpl, AccessOptions, AccessResponse},
+        request::RequestImpl,
+        Result,
+    },
+    desktop::HandleToken,
+    AppID, WindowIdentifierType,
+};
+use async_trait::async_trait;
+
+#[derive(Default)]
+pub struct Access;
+
+#[async_trait]
+impl RequestImpl for Access {
+    async fn close(&self, token: HandleToken) {
+        tracing::debug!("IN Close(): {token}");
+    }
+}
+
+#[async_trait]
+impl AccessImpl for Access {
+    async fn access_dialog(
+        &self,
+        _handle: HandleToken,
+        _app_id: Option<AppID>,
+        _window_identifier: Option<WindowIdentifierType>,
+        _title: String,
+        _subtitle: String,
+        _body: String,
+        options: AccessOptions,
+    ) -> Result<AccessResponse> {
+        let mut response = AccessResponse::default();
+        for choice in options.choices() {
+            response = response.choice(choice.id(), choice.initial_selection());
+        }
+        Ok(response)
+    }
+}
