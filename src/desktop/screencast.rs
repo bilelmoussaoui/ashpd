39db240d880a4ce4ad5893e4c6e5b15c6e36ae5a
Kamron Bhavnagri <kamwithk@tuta.io>
2025-06-29 20:52:45 +1000

    feat: add screen cast with gstreamer example

--- src/desktop/screencast.rs
+++ src/desktop/screencast.rs
@@ -34,7 +34,7 @@
 //!     Ok(())
 //! }
 //! ```
-//! An example on how to connect with Pipewire can be found [here](https://github.com/bilelmoussaoui/ashpd/blob/master/examples/screen_cast_pw.rs).
+//! An example on how to connect with Pipewire can be found [here](https://github.com/bilelmoussaoui/ashpd/blob/master/examples/screen_cast_pw.rs), and with GStreamer [here](https://github.com/bilelmoussaoui/ashpd/blob/master/examples/screen_cast_gstreamer.rs).
 
 use std::{collections::HashMap, fmt::Debug, os::fd::OwnedFd};
 
