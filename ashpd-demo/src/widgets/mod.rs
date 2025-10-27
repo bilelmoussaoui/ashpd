347bdcea641de1068f1d5c982cac59086e8263d6
Maximiliano Sandoval <msandova@protonmail.com>
2025-10-27 19:17:58 +0100

    demo: Remove SidebarRow
    Replaced by the AdwViewSwitcherSidebar built-in rows.

--- ashpd-demo/src/widgets/mod.rs
+++ ashpd-demo/src/widgets/mod.rs
@@ -3,11 +3,9 @@ mod gst_paintable;
 mod notification;
 mod portal_page;
 mod removable_row;
-mod sidebar_row;
 
 pub use color_widget::ColorWidget;
 pub use gst_paintable::CameraPaintable;
 pub use notification::{Notification, NotificationKind};
 pub use portal_page::{PortalPage, PortalPageExt, PortalPageImpl};
 pub use removable_row::RemovableRow;
-pub use sidebar_row::SidebarRow;
