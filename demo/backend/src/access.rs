e2ee69ea9311fdbbf78424611980785ea805e1c5
Maximiliano Sandoval <msandova@protonmail.com>
2025-12-25 23:58:13 +0100

    backend-demo: Port to ashpd-handles

--- demo/backend/src/access.rs
+++ demo/backend/src/access.rs
@@ -1,5 +1,4 @@
 use ashpd::{
-    AppID, WindowIdentifierType,
     backend::{
         Result,
         access::{AccessImpl, AccessOptions, AccessResponse},
@@ -7,6 +6,7 @@ use ashpd::{
     },
     desktop::HandleToken,
 };
+use ashpd_handles::{AppID, WindowIdentifierType};
 use async_trait::async_trait;
 
 #[derive(Default)]
