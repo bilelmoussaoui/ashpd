5d14d8f29c2afe0426f12c5bd0a129d811a14148
Bilal Elmoussaoui <belmouss@redhat.com>
2026-02-15 21:01:40 +0100

    client/demo: Add missing notification options

--- demo/client/src/portals/desktop/notification.rs
+++ demo/client/src/portals/desktop/notification.rs
@@ -1,10 +1,12 @@
+use std::{fs::File, os::fd::OwnedFd};
+
 use adw::{prelude::*, subclass::prelude::*};
 use ashpd::desktop::{
     Icon,
-    notification::{Button, Notification, NotificationProxy, Priority},
+    notification::{Button, Category, DisplayHint, Notification, NotificationProxy, Priority},
 };
 use futures_util::stream::StreamExt;
-use gtk::glib;
+use gtk::{gio, glib};
 
 use self::button::NotificationButton;
 use crate::{
@@ -13,6 +15,8 @@ use crate::{
 };
 
 mod imp {
+    use std::cell::RefCell;
+
     use super::*;
 
     #[derive(Debug, Default, gtk::CompositeTemplate)]
@@ -31,6 +35,22 @@ mod imp {
         #[template_child]
         pub priority_combo: TemplateChild<adw::ComboRow>,
         #[template_child]
+        pub category_combo: TemplateChild<adw::ComboRow>,
+        #[template_child]
+        pub transient_switch: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub tray_switch: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub persistent_switch: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub hide_on_lockscreen_switch: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub hide_content_on_lockscreen_switch: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub show_as_new_switch: TemplateChild<adw::SwitchRow>,
+        #[template_child]
+        pub sound_row: TemplateChild<adw::ActionRow>,
+        #[template_child]
         pub default_action_entry: TemplateChild<adw::EntryRow>,
         #[template_child]
         pub default_action_target_entry: TemplateChild<adw::EntryRow>,
@@ -44,6 +64,7 @@ mod imp {
         pub response_group: TemplateChild<adw::PreferencesGroup>,
         #[template_child]
         pub buttons_box: TemplateChild<gtk::Box>,
+        pub selected_sound: RefCell<Option<gio::File>>,
     }
 
     #[glib::object_subclass]
@@ -63,6 +84,15 @@ mod imp {
             klass.install_action("notification.add_button", None, |page, _, _| {
                 page.add_button();
             });
+            klass.install_action_async(
+                "notification.select_sound",
+                None,
+                |page, _, _| async move {
+                    if let Err(err) = page.select_sound().await {
+                        tracing::error!("Failed to select sound file {err}");
+                    }
+                },
+            );
         }
 
         fn instance_init(obj: &glib::subclass::InitializingObject<Self>) {
@@ -82,6 +112,30 @@ glib::wrapper! {
 }
 
 impl NotificationPage {
+    async fn select_sound(&self) -> Result<(), glib::Error> {
+        let imp = self.imp();
+        let filter = gtk::FileFilter::new();
+        filter.add_mime_type("audio/*");
+        filter.set_name(Some("Audio files"));
+
+        let filters = gio::ListStore::new::<gtk::FileFilter>();
+        filters.append(&filter);
+
+        let root = self.native().unwrap();
+        let file = gtk::FileDialog::builder()
+            .accept_label("Select")
+            .modal(true)
+            .title("Notification Sound")
+            .filters(&filters)
+            .build()
+            .open_future(root.downcast_ref::<gtk::Window>())
+            .await?;
+
+        imp.sound_row.set_subtitle(&file.uri());
+        imp.selected_sound.replace(Some(file));
+        Ok(())
+    }
+
     fn add_button(&self) {
         let button = NotificationButton::default();
         button.connect_removed(glib::clone!(
@@ -122,6 +176,41 @@ impl NotificationPage {
         let body = imp.body_entry.text();
         let markup_body = imp.markup_body_entry.text();
         let icon_name = imp.icon_entry.text();
+        let category = match imp.category_combo.selected() {
+            0 => None, // None
+            1 => Some(Category::ImMessage),
+            2 => Some(Category::AlarmRinging),
+            3 => Some(Category::IncomingCall),
+            4 => Some(Category::OngoingCall),
+            5 => Some(Category::MissedCall),
+            6 => Some(Category::ExtremeWeather),
+            7 => Some(Category::CellNetworkExtremeDanger),
+            8 => Some(Category::CellNetworkSevereDanger),
+            9 => Some(Category::CellNetworkAmberAlert),
+            10 => Some(Category::CellNetworkBroadcastTest),
+            11 => Some(Category::LowBattery),
+            12 => Some(Category::WebNotification),
+            _ => None,
+        };
+        let mut display_hints = Vec::new();
+        if imp.transient_switch.is_active() {
+            display_hints.push(DisplayHint::Transient);
+        }
+        if imp.tray_switch.is_active() {
+            display_hints.push(DisplayHint::Tray);
+        }
+        if imp.persistent_switch.is_active() {
+            display_hints.push(DisplayHint::Persistent);
+        }
+        if imp.hide_on_lockscreen_switch.is_active() {
+            display_hints.push(DisplayHint::HideOnLockScreen);
+        }
+        if imp.hide_content_on_lockscreen_switch.is_active() {
+            display_hints.push(DisplayHint::HideContentOnLockScreen);
+        }
+        if imp.show_as_new_switch.is_active() {
+            display_hints.push(DisplayHint::ShowAsNew);
+        }
         let default_action = imp.default_action_entry.text();
         let default_action_target = imp.default_action_target_entry.text();
         let priority = match imp.priority_combo.selected() {
@@ -142,7 +231,21 @@ impl NotificationPage {
             notification = notification.markup_body(&*markup_body);
         }
         if !icon_name.is_empty() {
-            notification = notification.icon(Icon::with_names(&[&icon_name]));
+            notification = notification.icon(Icon::with_names([&icon_name]));
+        }
+        if let Some(cat) = category {
+            notification = notification.category(cat);
+        }
+        if !display_hints.is_empty() {
+            notification = notification.display_hint(display_hints);
+        }
+
+        let selected_sound = imp.selected_sound.borrow().clone();
+        if let Some(sound_file) = selected_sound {
+            let path = sound_file.path().unwrap();
+            if let Ok(file) = File::open(path) {
+                notification = notification.sound(OwnedFd::from(file));
+            }
         }
 
         for button in self.buttons().into_iter() {
@@ -205,6 +308,7 @@ mod button {
             pub(super) label_row: adw::EntryRow,
             pub(super) action_row: adw::EntryRow,
             pub(super) target_row: adw::EntryRow,
+            pub(super) purpose_combo: adw::ComboRow,
         }
 
         #[glib::object_subclass]
@@ -254,7 +358,22 @@ mod button {
             let label = imp.label_row.text();
             let action = imp.action_row.text();
             let target = imp.target_row.text();
-            Button::new(&label, &action).target(&*target)
+            let purpose = match imp.purpose_combo.selected() {
+                0 => None, // None
+                1 => Some(ashpd::desktop::notification::ButtonPurpose::ImReplyWithText),
+                2 => Some(ashpd::desktop::notification::ButtonPurpose::CallAccept),
+                3 => Some(ashpd::desktop::notification::ButtonPurpose::CallDecline),
+                4 => Some(ashpd::desktop::notification::ButtonPurpose::CallHangup),
+                5 => Some(ashpd::desktop::notification::ButtonPurpose::CallEnableSpeakerphone),
+                6 => Some(ashpd::desktop::notification::ButtonPurpose::CallDisableSpeakerphone),
+                7 => Some(ashpd::desktop::notification::ButtonPurpose::SystemCustomAlert),
+                _ => None,
+            };
+            let mut button = Button::new(&label, &action).target(&*target);
+            if let Some(p) = purpose {
+                button = button.purpose(p);
+            }
+            button
         }
 
         fn create_widgets(&self) {
@@ -272,6 +391,19 @@ mod button {
             list_box.append(&imp.action_row);
             imp.target_row.set_title("Action Target");
             list_box.append(&imp.target_row);
+
+            imp.purpose_combo.set_title("Purpose");
+            imp.purpose_combo.set_model(Some(&gtk::StringList::new(&[
+                "None",
+                "IM Reply with Text",
+                "Call Accept",
+                "Call Decline",
+                "Call Hang Up",
+                "Call Enable Speakerphone",
+                "Call Disable Speakerphone",
+                "System Custom Alert",
+            ])));
+            list_box.append(&imp.purpose_combo);
 
             container.append(&list_box);
 
