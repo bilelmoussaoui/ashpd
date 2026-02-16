bilelmoussaoui/ashpd

commit 3c47378cab78d6e8f0240e3efcff6c4be3948440

client: Use Optional for WindowIdentifier

client/src/desktop/input_capture.rs
@@ -278,7 +278,7 @@ use zbus::zvariant::{
 };
 
 use super::{HandleToken, Request, Session, session::SessionPortal};
-use crate::{Error, WindowIdentifier, proxy::Proxy, window_identifier::MaybeWindowIdentifierExt};
+use crate::{Error, WindowIdentifier, proxy::Proxy};
 
 #[derive(Serialize_repr, Deserialize_repr, PartialEq, Eq, Debug, Copy, Clone, Type)]
 #[bitflags]
@@ -593,7 +593,7 @@ impl InputCapture {
             session_handle_token: Default::default(),
             capabilities,
         };
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         let (request, proxy) = futures_util::try_join!(
             self.0.request::<CreateSessionResponse>(
                 &options.handle_token,

client/src/desktop/account.rs
@@ -22,13 +22,10 @@
 //! }
 //! ```
 
-use zbus::zvariant::{DeserializeDict, SerializeDict, Type};
+use zbus::zvariant::{DeserializeDict, Optional, SerializeDict, Type};
 
 use super::HandleToken;
-use crate::{
-    Error, WindowIdentifier, desktop::request::Request, proxy::Proxy,
-    window_identifier::MaybeWindowIdentifierExt,
-};
+use crate::{Error, WindowIdentifier, desktop::request::Request, proxy::Proxy};
 
 #[derive(SerializeDict, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
@@ -103,12 +100,12 @@ impl AccountProxy {
         identifier: Option<&WindowIdentifier>,
         options: UserInformationOptions,
     ) -> Result<Request<UserInformation>, Error> {
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .request(
                 &options.handle_token,
                 "GetUserInformation",
-                (&identifier, &options),
+                (identifier, &options),
             )
             .await
     }

client/src/desktop/background.rs
@@ -31,10 +31,10 @@
 //! file](https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#introduction) will be used.
 
 use serde::Serialize;
-use zbus::zvariant::{DeserializeDict, SerializeDict, Type};
+use zbus::zvariant::{DeserializeDict, Optional, SerializeDict, Type};
 
 use super::{HandleToken, Request};
-use crate::{Error, WindowIdentifier, proxy::Proxy, window_identifier::MaybeWindowIdentifierExt};
+use crate::{Error, WindowIdentifier, proxy::Proxy};
 
 #[derive(SerializeDict, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
@@ -139,12 +139,12 @@ impl BackgroundProxy {
         identifier: Option<&WindowIdentifier>,
         options: BackgroundOptions,
     ) -> Result<Request<Background>, Error> {
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .request(
                 &options.handle_token,
                 "RequestBackground",
-                (&identifier, &options),
+                (identifier, &options),
             )
             .await
     }

client/src/desktop/dynamic_launcher.rs
@@ -55,13 +55,10 @@ use std::collections::HashMap;
 use enumflags2::{BitFlags, bitflags};
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
-use zbus::zvariant::{self, DeserializeDict, OwnedValue, SerializeDict, Type, Value};
+use zbus::zvariant::{self, DeserializeDict, Optional, OwnedValue, SerializeDict, Type, Value};
 
 use super::{HandleToken, Icon, Request};
-use crate::{
-    ActivationToken, Error, WindowIdentifier, proxy::Proxy,
-    window_identifier::MaybeWindowIdentifierExt,
-};
+use crate::{ActivationToken, Error, WindowIdentifier, proxy::Proxy};
 
 #[bitflags]
 #[derive(Default, Serialize_repr, Deserialize_repr, PartialEq, Eq, Debug, Copy, Clone, Type)]
@@ -270,7 +267,7 @@ impl DynamicLauncherProxy {
         if !icon.is_bytes() {
             return Err(UnexpectedIconError {}.into());
         }
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .request(
                 &options.handle_token,

client/src/desktop/email.rs
@@ -27,13 +27,10 @@
 use std::os::fd::OwnedFd;
 
 use serde::Serialize;
-use zbus::zvariant::{self, SerializeDict, Type};
+use zbus::zvariant::{self, Optional, SerializeDict, Type};
 
 use super::{HandleToken, Request};
-use crate::{
-    ActivationToken, Error, WindowIdentifier, proxy::Proxy,
-    window_identifier::MaybeWindowIdentifierExt,
-};
+use crate::{ActivationToken, Error, WindowIdentifier, proxy::Proxy};
 
 #[derive(SerializeDict, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
@@ -86,12 +83,12 @@ impl EmailProxy {
         identifier: Option<&WindowIdentifier>,
         options: EmailOptions,
     ) -> Result<Request<()>, Error> {
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .empty_request(
                 &options.handle_token,
                 "ComposeEmail",
-                &(&identifier, &options),
+                &(identifier, &options),
             )
             .await
     }

client/src/desktop/file_chooser.rs
@@ -83,12 +83,10 @@ use std::path::Path;
 
 use serde::{Deserialize, Serialize};
 use serde_repr::{Deserialize_repr, Serialize_repr};
-use zbus::zvariant::{DeserializeDict, SerializeDict, Type};
+use zbus::zvariant::{DeserializeDict, Optional, SerializeDict, Type};
 
 use super::{HandleToken, Request};
-use crate::{
-    Error, FilePath, WindowIdentifier, proxy::Proxy, window_identifier::MaybeWindowIdentifierExt,
-};
+use crate::{Error, FilePath, WindowIdentifier, proxy::Proxy};
 
 #[derive(Clone, Serialize, Deserialize, Type, Debug, PartialEq)]
 /// A file filter, to limit the available file choices to a mimetype or a glob
@@ -324,12 +322,12 @@ impl FileChooserProxy {
         title: &str,
         options: OpenFileOptions,
     ) -> Result<Request<SelectedFiles>, Error> {
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .request(
                 &options.handle_token,
                 "OpenFile",
-                &(&identifier, title, &options),
+                &(identifier, title, &options),
             )
             .await
     }
@@ -341,12 +339,12 @@ impl FileChooserProxy {
         title: &str,
         options: SaveFileOptions,
     ) -> Result<Request<SelectedFiles>, Error> {
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .request(
                 &options.handle_token,
                 "SaveFile",
-                &(&identifier, title, &options),
+                &(identifier, title, &options),
             )
             .await
     }
@@ -358,12 +356,12 @@ impl FileChooserProxy {
         title: &str,
         options: SaveFilesOptions,
     ) -> Result<Request<SelectedFiles>, Error> {
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .request(
                 &options.handle_token,
                 "SaveFiles",
-                &(&identifier, title, &options),
+                &(identifier, title, &options),
             )
             .await
     }

client/src/desktop/global_shortcuts.rs
@@ -5,13 +5,12 @@ use std::{collections::HashMap, fmt::Debug, time::Duration};
 use futures_util::Stream;
 use serde::{Deserialize, Serialize};
 use zbus::zvariant::{
-    DeserializeDict, ObjectPath, OwnedObjectPath, OwnedValue, SerializeDict, Type,
+    DeserializeDict, ObjectPath, Optional, OwnedObjectPath, OwnedValue, SerializeDict, Type,
 };
 
 use super::{HandleToken, Request, Session, session::SessionPortal};
 use crate::{
-    ActivationToken, Error, WindowIdentifier, desktop::session::CreateSessionResponse,
-    proxy::Proxy, window_identifier::MaybeWindowIdentifierExt,
+    ActivationToken, Error, WindowIdentifier, desktop::session::CreateSessionResponse, proxy::Proxy,
 };
 
 #[derive(Clone, SerializeDict, Type, Debug, Default)]
@@ -270,7 +269,7 @@ impl GlobalShortcuts {
         identifier: Option<&WindowIdentifier>,
     ) -> Result<Request<BindShortcuts>, Error> {
         let options = BindShortcutsOptions::default();
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .request(
                 &options.handle_token,
@@ -312,7 +311,7 @@ impl GlobalShortcuts {
         let options = ConfigureShortcutsOptions {
             activation_token: activation_token.into(),
         };
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
 
         self.0
             .call_versioned::<()>("ConfigureShortcuts", &(session, identifier, options), 2)

client/src/desktop/inhibit.rs
@@ -38,13 +38,10 @@ use enumflags2::{BitFlags, bitflags};
 use futures_util::Stream;
 use serde::Deserialize;
 use serde_repr::{Deserialize_repr, Serialize_repr};
-use zbus::zvariant::{DeserializeDict, ObjectPath, OwnedObjectPath, SerializeDict, Type};
+use zbus::zvariant::{DeserializeDict, ObjectPath, Optional, OwnedObjectPath, SerializeDict, Type};
 
 use super::{HandleToken, Request, Session, session::SessionPortal};
-use crate::{
-    Error, WindowIdentifier, desktop::session::CreateSessionResponse, proxy::Proxy,
-    window_identifier::MaybeWindowIdentifierExt,
-};
+use crate::{Error, WindowIdentifier, desktop::session::CreateSessionResponse, proxy::Proxy};
 
 #[derive(SerializeDict, Type, Debug, Default)]
 /// Specified options for a [`InhibitProxy::create_monitor`] request.
@@ -175,8 +172,8 @@ impl InhibitProxy {
         identifier: Option<&WindowIdentifier>,
     ) -> Result<Session<Self>, Error> {
         let options = CreateMonitorOptions::default();
-        let identifier = identifier.to_string_or_empty();
-        let body = &(&identifier, &options);
+        let identifier = Optional::from(identifier);
+        let body = &(identifier, &options);
         let (monitor, proxy) = futures_util::try_join!(
             self.0
                 .request::<CreateSessionResponse>(&options.handle_token, "CreateMonitor", body),
@@ -209,12 +206,12 @@ impl InhibitProxy {
             reason: Some(reason.to_owned()),
             handle_token: Default::default(),
         };
-        let identifier = identifier.to_string_or_empty();
+        let identifier = Optional::from(identifier);
         self.0
             .empty_request(
                 &options.handle_token,
                 "Inhibit",
-                &(&identifier, flags, &options),
+                &(identifier, flags, &options),
             )
             .await
     }

commit 55c6844a99de47753231c1aa8c753ebbe6a81572

inputcapture: Fix build error due to missing Optional
$ cargo build --all-targets --features=input_capture
   Compiling ashpd v0.13.0-alpha (/path/to/ashpd/client)
error[E0433]: failed to resolve: use of undeclared type `Optional`
   --> client/src/desktop/input_capture.rs:596:26
    |
596 |         let identifier = Optional::from(identifier);
    |                          ^^^^^^^^ use of undeclared type `Optional`

Fixes: 3c47378cab78 ("client: Use Optional for WindowIdentifier")

client/src/desktop/input_capture.rs
@@ -274,7 +274,8 @@ use futures_util::Stream;
 use serde::{Deserialize, de::Visitor};
 use serde_repr::{Deserialize_repr, Serialize_repr};
 use zbus::zvariant::{
-    self, DeserializeDict, ObjectPath, OwnedObjectPath, OwnedValue, SerializeDict, Type, Value,
+    self, DeserializeDict, ObjectPath, Optional, OwnedObjectPath, OwnedValue, SerializeDict, Type,
+    Value,
 };
 
 use super::{HandleToken, Request, Session, session::SessionPortal};