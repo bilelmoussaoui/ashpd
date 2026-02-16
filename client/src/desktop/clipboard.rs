bilelmoussaoui/ashpd

commit ec8af6ed589d7fc4d29a2a73b788050aa5cf06fd

Get rid of SerializeDict/DeserializeDict usages
Replace them with plain serde, that allows to use features like
default/skip_serialize_if and so on

client/src/desktop/clipboard.rs
@@ -6,22 +6,27 @@
 use std::collections::HashMap;
 
 use futures_util::{Stream, StreamExt};
-use zbus::zvariant::{DeserializeDict, OwnedFd, OwnedObjectPath, SerializeDict, Type, Value};
+use serde::{Deserialize, Serialize};
+use zbus::zvariant::{OwnedFd, OwnedObjectPath, Type, Value, as_value::optional};
 
 use super::{Session, remote_desktop::RemoteDesktop};
 use crate::{Result, proxy::Proxy};
 
-#[derive(Debug, Type, SerializeDict)]
+#[derive(Debug, Type, Serialize)]
 #[zvariant(signature = "dict")]
 struct SetSelectionOptions<'a> {
+    #[serde(borrow)]
+    #[serde(with = "as_value", skip_serializing_if = "Vec::is_empty")]
     mime_types: &'a [&'a str],
 }
 
-#[derive(Debug, Type, DeserializeDict)]
+#[derive(Debug, Type, Deserialize, Default)]
 #[zvariant(signature = "dict")]
 /// The details of a new clipboard selection.
 pub struct SelectionOwnerChanged {
-    mime_types: Option<Vec<String>>,
+    #[serde(default, with = "as_value")]
+    mime_types: Vec<String>,
+    #[serde(default, with = "optional")]
     session_is_owner: Option<bool>,
 }
 
@@ -32,8 +37,8 @@ impl SelectionOwnerChanged {
     }
 
     /// A list of mime types the new clipboard has content for.
-    pub fn mime_types(&self) -> Vec<String> {
-        self.mime_types.clone().unwrap_or_default()
+    pub fn mime_types(&self) -> &[String] {
+        &self.mime_types
     }
 }
 

client/src/backend/access.rs
@@ -1,7 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use serde::Deserialize;
+use serde::{Deserialize, Serialize};
 
 use crate::{
     AppID, WindowIdentifierType,
@@ -10,16 +10,21 @@ use crate::{
         request::{Request, RequestImpl},
     },
     desktop::{HandleToken, Icon, request::Response},
-    zvariant::{self, DeserializeDict, Optional, OwnedObjectPath, SerializeDict},
+    zvariant::{self, Optional, OwnedObjectPath, as_value::optional},
 };
 
-#[derive(DeserializeDict, zvariant::Type)]
+#[derive(Deserialize, zvariant::Type)]
 #[zvariant(signature = "dict")]
 pub struct AccessOptions {
+    #[serde(default, with = "optional")]
     modal: Option<bool>,
+    #[serde(default, with = "optional")]
     deny_label: Option<String>,
+    #[serde(default, with = "optional")]
     grant_label: Option<String>,
+    #[serde(default, with = "optional")]
     icon: Option<String>,
+    #[serde(default, with = "optional")]
     choices: Option<Vec<Choice>>,
 }
 
@@ -73,9 +78,10 @@ impl AccessOptions {
     }
 }
 
-#[derive(SerializeDict, Debug, zvariant::Type, Default)]
+#[derive(Serialize, Debug, zvariant::Type, Default)]
 #[zvariant(signature = "dict")]
 pub struct AccessResponse {
+    #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     choices: Option<Vec<(String, String)>>,
 }
 

client/src/backend/account.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use serde::Deserialize;
 
 use crate::{
     AppID, WindowIdentifierType,
@@ -9,12 +10,13 @@ use crate::{
         request::{Request, RequestImpl},
     },
     desktop::{HandleToken, account::UserInformation, request::Response},
-    zvariant::{DeserializeDict, Optional, OwnedObjectPath, Type},
+    zvariant::{Optional, OwnedObjectPath, Type, as_value::optional},
 };
 
-#[derive(Debug, DeserializeDict, Type)]
+#[derive(Debug, Deserialize, Type)]
 #[zvariant(signature = "dict")]
 pub struct UserInformationOptions {
+    #[serde(default, with = "optional")]
     reason: Option<String>,
 }
 

client/src/backend/app_chooser.rs
@@ -1,13 +1,16 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
-use serde::{Deserialize, Deserializer};
+use serde::{Deserialize, Deserializer, Serialize};
 
 use crate::{
     ActivationToken, AppID, PortalError, WindowIdentifierType,
     backend::request::{Request, RequestImpl},
     desktop::{HandleToken, Response},
-    zvariant::{DeserializeDict, Optional, OwnedObjectPath, SerializeDict, Type},
+    zvariant::{
+        Optional, OwnedObjectPath, Type,
+        as_value::{self, optional},
+    },
 };
 
 /// The desktop ID of an application.
@@ -44,14 +47,20 @@ impl std::fmt::Display for DesktopID {
     }
 }
 
-#[derive(Debug, DeserializeDict, Type)]
+#[derive(Debug, Deserialize, Type)]
 #[zvariant(signature = "dict")]
 pub struct ChooserOptions {
+    #[serde(default, with = "optional")]
     last_choice: Option<DesktopID>,
+    #[serde(default, with = "optional")]
     modal: Option<bool>,
+    #[serde(default, with = "optional")]
     content_type: Option<String>,
+    #[serde(default, with = "optional")]
     uri: Option<String>,
+    #[serde(default, with = "optional")]
     filename: Option<String>,
+    #[serde(default, with = "optional")]
     activation_token: Option<ActivationToken>,
 }
 
@@ -81,10 +90,12 @@ impl ChooserOptions {
     }
 }
 
-#[derive(Debug, SerializeDict, Type)]
+#[derive(Debug, Serialize, Type)]
 #[zvariant(signature = "dict")]
 pub struct Choice {
+    #[serde(with = "as_value")]
     choice: AppID,
+    #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     activation_token: Option<ActivationToken>,
 }
 

client/src/backend/background.rs
@@ -2,6 +2,7 @@ use std::{collections::HashMap, sync::Arc};
 
 use async_trait::async_trait;
 use enumflags2::{BitFlags, bitflags};
+use serde::Serialize;
 use serde_repr::{Deserialize_repr, Serialize_repr};
 
 use crate::{
@@ -9,7 +10,7 @@ use crate::{
     backend::request::{Request, RequestImpl},
     desktop::{HandleToken, Response},
     zbus::object_server::SignalEmitter,
-    zvariant::{OwnedObjectPath, SerializeDict, Type},
+    zvariant::{OwnedObjectPath, Type, as_value},
 };
 
 #[derive(Serialize_repr, Deserialize_repr, Copy, Clone, PartialEq, Eq, Debug, Type)]
@@ -20,9 +21,10 @@ pub enum Activity {
     AllowInstance = 2,
 }
 
-#[derive(Debug, SerializeDict, Type)]
+#[derive(Debug, Serialize, Type)]
 #[zvariant(signature = "dict")]
 pub struct Background {
+    #[serde(with = "as_value")]
     result: Activity,
 }
 

client/src/backend/email.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use serde::Deserialize;
 
 use crate::{
     ActivationToken, AppID, Uri, WindowIdentifierType,
@@ -9,19 +10,27 @@ use crate::{
         request::{Request, RequestImpl},
     },
     desktop::{HandleToken, request::Response},
-    zvariant::{self, DeserializeDict, Optional, OwnedObjectPath},
+    zvariant::{self, Optional, OwnedObjectPath, as_value::optional},
 };
 
-#[derive(DeserializeDict, zvariant::Type)]
+#[derive(Deserialize, zvariant::Type)]
 #[zvariant(signature = "dict")]
 pub struct Options {
+    #[serde(default, with = "optional")]
     address: Option<String>,
+    #[serde(default, with = "optional")]
     addresses: Option<Vec<String>>,
+    #[serde(default, with = "optional")]
     cc: Option<Vec<String>>,
+    #[serde(default, with = "optional")]
     bcc: Option<Vec<String>>,
+    #[serde(default, with = "optional")]
     subject: Option<String>,
+    #[serde(default, with = "optional")]
     body: Option<String>,
+    #[serde(default, with = "optional")]
     attachments: Option<Vec<Uri>>,
+    #[serde(default, with = "optional")]
     activation_token: Option<ActivationToken>,
 }
 

client/src/backend/file_chooser.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use serde::{Deserialize, Serialize};
 
 use crate::{
     AppID, FilePath, Uri, WindowIdentifierType,
@@ -13,17 +14,24 @@ use crate::{
         file_chooser::{Choice, FileFilter},
         request::Response,
     },
-    zvariant::{DeserializeDict, Optional, OwnedObjectPath, SerializeDict, Type},
+    zvariant::{
+        Optional, OwnedObjectPath, Type,
+        as_value::{self, optional},
+    },
 };
 
-#[derive(Debug, Type, SerializeDict, Default)]
+#[derive(Debug, Type, Serialize, Default)]
 #[zvariant(signature = "dict")]
 pub struct SelectedFiles {
+    #[serde(with = "as_value", skip_serializing_if = "Vec::is_empty")]
     uris: Vec<Uri>,
+    #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     choices: Option<Vec<(String, String)>>,
     // Not relevant for SaveFiles
+    #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     current_filter: Option<FileFilter>,
     // Only relevant for OpenFile
+    #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     writable: Option<bool>,
 }
 
@@ -54,16 +62,24 @@ impl SelectedFiles {
 // but we will have to figure out how to handle handle_token
 // as if we set it to Option<T>, the Default would no longer
 // generate a random value, breaking some of the infrastructure we had
-#[derive(DeserializeDict, Type, Debug)]
+#[derive(Deserialize, Type, Debug)]
 #[zvariant(signature = "dict")]
 pub struct OpenFileOptions {
+    #[serde(default, with = "optional")]
     accept_label: Option<String>,
+    #[serde(default, with = "optional")]
     modal: Option<bool>,
+    #[serde(default, with = "optional")]
     multiple: Option<bool>,
+    #[serde(default, with = "optional")]
     directory: Option<bool>,
+    #[serde(default, with = "optional")]
     filters: Option<Vec<FileFilter>>,
+    #[serde(default, with = "optional")]
     current_filter: Option<FileFilter>,
+    #[serde(default, with = "optional")]
     choices: Option<Vec<Choice>>,
+    #[serde(default, with = "optional")]
     current_folder: Option<FilePath>,
 }
 
@@ -101,17 +117,26 @@ impl OpenFileOptions {
     }
 }
 
-#[derive(DeserializeDict, Type, Debug)]
+#[derive(Deserialize, Type, Debug)]
 #[zvariant(signature = "dict")]
 pub struct SaveFileOptions {
+    #[serde(default, with = "optional")]
     accept_label: Option<String>,
+    #[serde(default, with = "optional")]
     modal: Option<bool>,
+    #[serde(default, with = "optional")]
     multiple: Option<bool>,
+    #[serde(default, with = "optional")]
     filters: Option<Vec<FileFilter>>,
+    #[serde(default, with = "optional")]
     current_filter: Option<FileFilter>,
+    #[serde(default, with = "optional")]
     choices: Option<Vec<Choice>>,
+    #[serde(default, with = "optional")]
     current_name: Option<String>,
+    #[serde(default, with = "optional")]
     current_folder: Option<FilePath>,
+    #[serde(default, with = "optional")]
     current_file: Option<FilePath>,
 }
 
@@ -153,13 +178,18 @@ impl SaveFileOptions {
     }
 }
 
-#[derive(DeserializeDict, Type, Debug)]
+#[derive(Deserialize, Type, Debug)]
 #[zvariant(signature = "dict")]
 pub struct SaveFilesOptions {
+    #[serde(default, with = "optional")]
     accept_label: Option<String>,
+    #[serde(default, with = "optional")]
     modal: Option<bool>,
+    #[serde(default, with = "optional")]
     choices: Option<Vec<Choice>>,
+    #[serde(default, with = "optional")]
     current_folder: Option<FilePath>,
+    #[serde(default, with = "optional")]
     files: Option<Vec<FilePath>>,
 }
 

client/src/backend/print.rs
@@ -1,6 +1,7 @@
 use std::sync::Arc;
 
 use async_trait::async_trait;
+use serde::Deserialize;
 
 use crate::{
     AppID, WindowIdentifierType,
@@ -13,13 +14,15 @@ use crate::{
         print::{PageSetup, PreparePrint, Settings},
         request::Response,
     },
-    zvariant::{self, DeserializeDict, Optional, OwnedObjectPath},
+    zvariant::{self, Optional, OwnedObjectPath, as_value::optional},
 };
 
-#[derive(DeserializeDict, zvariant::Type)]
+#[derive(Deserialize, zvariant::Type)]
 #[zvariant(signature = "dict")]
 pub struct PreparePrintOptions {
+    #[serde(default, with = "optional")]
     modal: Option<bool>,
+    #[serde(default, with = "optional")]
     accept_label: Option<String>,
 }
 
@@ -33,10 +36,12 @@ impl PreparePrintOptions {
     }
 }
 
-#[derive(DeserializeDict, zvariant::Type)]
+#[derive(Deserialize, zvariant::Type)]
 #[zvariant(signature = "dict")]
 pub struct PrintOptions {
+    #[serde(default, with = "optional")]
     modal: Option<bool>,
+    #[serde(default, with = "optional")]
     token: Option<u32>,
 }
 

commit 0190228dd1ef69a9d86f9e35abad6525e2113f92

clipboard: Fix build errors caused by new serialization
error[E0433]: failed to resolve: use of unresolved module or unlinked crate `as_value`
  --> client/src/desktop/clipboard.rs:19:20
   |
19 |     #[serde(with = "as_value", skip_serializing_if = "Vec::is_empty")]
   |                    ^^^^^^^^^^ use of unresolved module or unlinked crate `as_value`

error[E0308]: mismatched types
  --> client/src/desktop/clipboard.rs:15:23
   |
15 | #[derive(Debug, Type, Serialize)]
   |                       ^^^^^^^^^ expected `&Vec<_, _>`, found `&&[&str]`
...
19 |     #[serde(with = "as_value", skip_serializing_if = "Vec::is_empty")]
   |                                                      --------------- arguments to this function are incorrect

Fixes: ec8af6ed589d ("Get rid of SerializeDict/DeserializeDict usages")

client/src/desktop/clipboard.rs
@@ -7,7 +7,7 @@ use std::collections::HashMap;
 
 use futures_util::{Stream, StreamExt};
 use serde::{Deserialize, Serialize};
-use zbus::zvariant::{OwnedFd, OwnedObjectPath, Type, Value, as_value::optional};
+use zbus::zvariant::{OwnedFd, OwnedObjectPath, Type, Value, as_value, as_value::optional};
 
 use super::{Session, remote_desktop::RemoteDesktop};
 use crate::{Result, proxy::Proxy};
@@ -16,7 +16,7 @@ use crate::{Result, proxy::Proxy};
 #[zvariant(signature = "dict")]
 struct SetSelectionOptions<'a> {
     #[serde(borrow)]
-    #[serde(with = "as_value", skip_serializing_if = "Vec::is_empty")]
+    #[serde(with = "as_value", skip_serializing_if = "<[_]>::is_empty")]
     mime_types: &'a [&'a str],
 }
 