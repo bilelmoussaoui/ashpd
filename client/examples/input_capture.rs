72fe251f51813461f124170094739acf1a7f4144
Bilal Elmoussaoui <belmouss@redhat.com>
2026-02-21 15:22:02 +0100

    examples: Adjust based on recent API changes

--- client/examples/input_capture.rs
+++ client/examples/input_capture.rs
@@ -1,22 +1,30 @@
-use ashpd::desktop::input_capture::{Barrier, BarrierID, Capabilities, InputCapture};
-use futures_util::StreamExt;
 use std::time::Duration;
+
+use ashpd::desktop::input_capture::{
+    Barrier, BarrierID, Capabilities, CreateSessionOptions, InputCapture, ReleaseOptions,
+};
+use futures_util::StreamExt;
 
 #[tokio::main]
 async fn main() -> ashpd::Result<()> {
     let input_capture = InputCapture::new().await?;
 
     let (session, _capabilities) = input_capture
-        .create_session(None, Capabilities::Keyboard | Capabilities::Pointer)
+        .create_session(
+            None,
+            CreateSessionOptions::default()
+                .set_capabilities(Capabilities::Keyboard | Capabilities::Pointer),
+        )
         .await?;
 
-    let zones_response = input_capture.zones(&session).await?;
+    let zones_response = input_capture.zones(&session, Default::default()).await?;
     let zones = zones_response.response()?;
     println!("Available Zones: {:?}", zones);
 
-    // Set up barriers on all edges of each zone, even the overlapping ones (if we have multiple
-    // zones). Those will fail and that's fine. The alternative is a more sophisticated algorithm
-    // to only set up barriers on the outside edges.
+    // Set up barriers on all edges of each zone, even the overlapping ones (if we
+    // have multiple zones). Those will fail and that's fine. The alternative is
+    // a more sophisticated algorithm to only set up barriers on the outside
+    // edges.
     let barriers: Vec<Barrier> =
         zones
             .regions()
@@ -58,7 +66,7 @@ async fn main() -> ashpd::Result<()> {
             .collect();
 
     let barriers_response = input_capture
-        .set_pointer_barriers(&session, &barriers, zones.zone_set())
+        .set_pointer_barriers(&session, &barriers, zones.zone_set(), Default::default())
         .await?;
 
     let result = barriers_response.response()?;
@@ -72,11 +80,13 @@ async fn main() -> ashpd::Result<()> {
 
     // Actual input events will be coming from the EIS file descriptor, once
     // any barrier activates.
-    let eifd = input_capture.connect_to_eis(&session).await?;
+    let eifd = input_capture
+        .connect_to_eis(&session, Default::default())
+        .await?;
     println!("Connected to EIS, fd: {:?}", eifd);
 
     // Enable input capture so the barriers can trigger
-    input_capture.enable(&session).await?;
+    input_capture.enable(&session, Default::default()).await?;
     println!("Input capture enabled - move cursor to any edge to trigger");
 
     // Listen for activation events, once we get the event we'll see EI events
@@ -102,13 +112,13 @@ async fn main() -> ashpd::Result<()> {
                 tokio::time::sleep(Duration::from_secs(5)).await;
 
                 // Release the input capture
-                input_capture.release(&session, activated.activation_id(), None).await?;
+                input_capture.release(&session, ReleaseOptions::default().set_activation_id(activated.activation_id())).await?;
                 println!("  Released input capture");
 
                 // And re-enable so we can play this game again. But wait a second first
                 // to give the user a chance to move away from the barrier.
                 tokio::time::sleep(Duration::from_secs(1)).await;
-                input_capture.enable(&session).await?;
+                input_capture.enable(&session, Default::default()).await?;
             }
             Some(deactivated) = deactivated_stream.next() => {
                 println!("Input capture DEACTIVATED");
