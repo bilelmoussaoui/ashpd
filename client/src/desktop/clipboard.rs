bilelmoussaoui/ashpd

commit effaceb359ca8bc6ef79df6ba6224d7f69a23b1d

Always use an Options struct
That makes adding new attributes not an API break change

client/src/desktop/clipboard.rs
@@ -3,22 +3,26 @@
 //! The portal is mostly meant to be used along with
 //! [`RemoteDesktop`]
 
-use std::collections::HashMap;
-
 use futures_util::{Stream, StreamExt};
 use serde::{Deserialize, Serialize};
-use zbus::zvariant::{OwnedFd, OwnedObjectPath, Type, Value, as_value, as_value::optional};
+use zbus::zvariant::{OwnedFd, OwnedObjectPath, Type, as_value, as_value::optional};
 
 use super::{Session, remote_desktop::RemoteDesktop};
 use crate::{Result, proxy::Proxy};
 
-#[derive(Debug, Type, Serialize)]
+#[derive(Debug, Type, Serialize, Default)]
 #[zvariant(signature = "dict")]
-struct SetSelectionOptions<'a> {
+/// Specified options for a [`Clipboard::set_selection`] request.
+pub struct SetSelectionOptions<'a> {
     #[serde(borrow)]
     #[serde(with = "as_value", skip_serializing_if = "<[_]>::is_empty")]
     mime_types: &'a [&'a str],
 }
+
+#[derive(Debug, Type, Serialize, Default)]
+#[zvariant(signature = "dict")]
+/// Specified options for a [`Clipboard::request`] request.
+pub struct RequestClipboardOptions {}
 
 #[derive(Debug, Type, Deserialize, Default)]
 #[zvariant(signature = "dict")]
@@ -71,8 +75,11 @@ impl Clipboard {
     ///
     /// See also [`RequestClipboard`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Clipboard.html#org-freedesktop-portal-clipboard-requestclipboard).
     #[doc(alias = "RequestClipboard")]
-    pub async fn request(&self, session: &Session<RemoteDesktop>) -> Result<()> {
-        let options: HashMap<&str, Value<'_>> = HashMap::default();
+    pub async fn request(
+        &self,
+        session: &Session<RemoteDesktop>,
+        options: RequestClipboardOptions,
+    ) -> Result<()> {
         self.0
             .call_method("RequestClipboard", &(session, options))
             .await?;
@@ -83,12 +90,11 @@ impl Clipboard {
     ///
     /// See also [`SetSelection`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Clipboard.html#org-freedesktop-portal-clipboard-setselection).
     #[doc(alias = "SetSelection")]
-    pub async fn set_selection(
+    pub async fn set_selection<'a>(
         &self,
         session: &Session<RemoteDesktop>,
-        mime_types: &[&str],
+        options: SetSelectionOptions<'a>,
     ) -> Result<()> {
-        let options = SetSelectionOptions { mime_types };
         self.0
             .call::<()>("SetSelection", &(session, options))
             .await?;

client/examples/screen_cast_gstreamer.rs
@@ -1,19 +1,22 @@
 use std::os::fd::{AsRawFd, OwnedFd};
 
-use ashpd::desktop::screencast::{CursorMode, Screencast, SourceType, Stream};
+use ashpd::desktop::screencast::{
+    CursorMode, Screencast, SelectSourcesOptions, SourceType, Stream,
+};
 use gst::prelude::*;
 
 async fn open_portal() -> ashpd::Result<(Stream, OwnedFd)> {
     let proxy = Screencast::new().await?;
-    let session = proxy.create_session().await?;
+    let session = proxy.create_session(Default::default()).await?;
     proxy
         .select_sources(
             &session,
-            CursorMode::Embedded,
-            SourceType::Monitor | SourceType::Window | SourceType::Virtual,
-            false,
-            None,
-            ashpd::desktop::PersistMode::ExplicitlyRevoked,
+            SelectSourcesOptions::default()
+                .cursor_mode(CursorMode::Embedded)
+                .sources(SourceType::Monitor | SourceType::Window | SourceType::Virtual)
+                .multiple(false)
+                .restore_token(None)
+                .persist_mode(ashpd::desktop::PersistMode::ExplicitlyRevoked),
         )
         .await?;
 
@@ -24,7 +27,9 @@ async fn open_portal() -> ashpd::Result<(Stream, OwnedFd)> {
         .expect("No stream found or selected")
         .to_owned();
 
-    let fd = proxy.open_pipe_wire_remote(&session).await?;
+    let fd = proxy
+        .open_pipe_wire_remote(&session, Default::default())
+        .await?;
 
     Ok((stream, fd))
 }

client/examples/screen_cast_pw.rs
@@ -2,7 +2,9 @@ use std::os::fd::{IntoRawFd, OwnedFd};
 
 use ashpd::desktop::{
     PersistMode,
-    screencast::{CursorMode, Screencast, SourceType, Stream as ScreencastStream},
+    screencast::{
+        CursorMode, Screencast, SelectSourcesOptions, SourceType, Stream as ScreencastStream,
+    },
 };
 use pipewire as pw;
 use pw::{properties::properties, spa};
@@ -13,15 +15,16 @@ struct UserData {
 
 async fn open_portal() -> ashpd::Result<(ScreencastStream, OwnedFd)> {
     let proxy = Screencast::new().await?;
-    let session = proxy.create_session().await?;
+    let session = proxy.create_session(Default::default()).await?;
     proxy
         .select_sources(
             &session,
-            CursorMode::Hidden,
-            SourceType::Monitor.into(),
-            false,
-            None,
-            PersistMode::DoNot,
+            SelectSourcesOptions::default()
+                .cursor_mode(CursorMode::Hidden)
+                .sources(SourceType::Monitor)
+                .multiple(false)
+                .restore_token(None)
+                .persist_mode(PersistMode::DoNot),
         )
         .await?;
 
@@ -32,7 +35,9 @@ async fn open_portal() -> ashpd::Result<(ScreencastStream, OwnedFd)> {
         .expect("no stream found / selected")
         .to_owned();
 
-    let fd = proxy.open_pipe_wire_remote(&session).await?;
+    let fd = proxy
+        .open_pipe_wire_remote(&session, Default::default())
+        .await?;
 
     Ok((stream, fd))
 }

client/src/desktop/background.rs
@@ -41,7 +41,8 @@ use crate::{Error, WindowIdentifier, proxy::Proxy};
 
 #[derive(Serialize, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
-struct BackgroundOptions {
+/// Specified options for a [`BackgroundProxy::request_background`] request.
+pub struct BackgroundRequestOptions {
     #[serde(with = "as_value")]
     handle_token: HandleToken,
     #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
@@ -60,6 +61,41 @@ struct BackgroundOptions {
         skip_serializing_if = "Vec::is_empty"
     )]
     command: Vec<String>,
+}
+
+impl BackgroundRequestOptions {
+    #[must_use]
+    /// Sets whether to auto start the application or not.
+    pub fn auto_start(mut self, auto_start: impl Into<Option<bool>>) -> Self {
+        self.autostart = auto_start.into();
+        self
+    }
+
+    #[must_use]
+    /// Sets whether the application is dbus activatable.
+    pub fn dbus_activatable(mut self, dbus_activatable: impl Into<Option<bool>>) -> Self {
+        self.dbus_activatable = dbus_activatable.into();
+        self
+    }
+
+    #[must_use]
+    /// Specifies the command line to execute.
+    /// If this is not specified, the [`Exec`](https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#exec-variables) line from the [desktop
+    /// file](https://specifications.freedesktop.org/desktop-entry-spec/desktop-entry-spec-latest.html#introduction)
+    pub fn command<P: IntoIterator<Item = I>, I: AsRef<str> + Type + Serialize>(
+        mut self,
+        command: P,
+    ) -> Self {
+        self.command = command.into_iter().map(|s| s.as_ref().to_owned()).collect();
+        self
+    }
+
+    #[must_use]
+    /// Sets a user-visible reason for the request.
+    pub fn reason<'a>(mut self, reason: impl Into<Option<&'a str>>) -> Self {
+        self.reason = reason.into().map(ToOwned::to_owned);
+        self
+    }
 }
 
 #[derive(Deserialize, Type, Debug)]
@@ -94,9 +130,18 @@ impl Background {
 
 #[derive(Serialize, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
-struct SetStatusOptions {
+/// Specified options for a [`BackgroundProxy::set_status`] request.
+pub struct SetStatusOptions {
     #[serde(with = "as_value")]
     message: String,
+}
+
+impl SetStatusOptions {
+    /// Sets the message to be displayed to the user.
+    pub fn set_message(mut self, message: &str) -> Self {
+        self.message = message.to_owned();
+        self
+    }
 }
 
 /// The interface lets sandboxed applications request that the application
@@ -143,23 +188,20 @@ impl BackgroundProxy {
     ///
     /// See also [`SetStatus`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Background.html#org-freedesktop-portal-background-setstatus).
     #[doc(alias = "SetStatus")]
-    pub async fn set_status(&self, message: &str) -> Result<(), Error> {
-        self.0
-            .call_versioned(
-                "SetStatus",
-                &(SetStatusOptions {
-                    message: message.to_owned(),
-                }),
-                2,
-            )
-            .await
+    pub async fn set_status(&self, options: SetStatusOptions) -> Result<(), Error> {
+        self.0.call_versioned("SetStatus", &(options), 2).await
     }
 
+    ///  Request background access.
+    ///
+    /// # Specifications
+    ///
+    /// See also [`RequestBackground`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Background.html#org-freedesktop-portal-background-requestbackground).
     #[doc(alias = "RequestBackground")]
-    async fn request_background(
+    pub async fn request_background(
         &self,
         identifier: Option<&WindowIdentifier>,
-        options: BackgroundOptions,
+        options: BackgroundRequestOptions,
     ) -> Result<Request<Background>, Error> {
         let identifier = Optional::from(identifier);
         self.0
@@ -187,7 +229,7 @@ impl std::ops::Deref for BackgroundProxy {
 #[derive(Debug, Default)]
 pub struct BackgroundRequest {
     identifier: Option<WindowIdentifier>,
-    options: BackgroundOptions,
+    options: BackgroundRequestOptions,
 }
 
 impl BackgroundRequest {

client/src/desktop/camera.rs
@@ -34,17 +34,23 @@
 use std::{collections::HashMap, os::fd::OwnedFd};
 
 use serde::Serialize;
-use zbus::zvariant::{self, Type, Value, as_value};
+use zbus::zvariant::{self, Type, as_value};
 
 use super::{HandleToken, Request};
 use crate::{Error, proxy::Proxy};
 
 #[derive(Serialize, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
-struct CameraAccessOptions {
+/// Specified options for a [`Camera::request_access`] request.
+pub struct CameraAccessOptions {
     #[serde(with = "as_value")]
     handle_token: HandleToken,
 }
+
+#[derive(Serialize, Type, Debug, Default)]
+#[zvariant(signature = "dict")]
+/// Specified options for a [`Camera::open_pipe_wire_remote`] request.
+pub struct OpenPipeWireRemoteOptions {}
 
 /// The interface lets sandboxed applications access camera devices, such as web
 /// cams.
@@ -80,8 +86,7 @@ impl Camera {
     /// See also [`AccessCamera`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Camera.html#org-freedesktop-portal-camera-accesscamera).
     #[doc(alias = "AccessCamera")]
     #[doc(alias = "xdp_portal_access_camera")]
-    pub async fn request_access(&self) -> Result<Request<()>, Error> {
-        let options = CameraAccessOptions::default();
+    pub async fn request_access(&self, options: CameraAccessOptions) -> Result<Request<()>, Error> {
         self.0
             .empty_request(&options.handle_token, "AccessCamera", &options)
             .await
@@ -99,10 +104,10 @@ impl Camera {
     /// See also [`OpenPipeWireRemote`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.Camera.html#org-freedesktop-portal-camera-openpipewireremote).
     #[doc(alias = "OpenPipeWireRemote")]
     #[doc(alias = "xdp_portal_open_pipewire_remote_for_camera")]
-    pub async fn open_pipe_wire_remote(&self) -> Result<OwnedFd, Error> {
-        // `options` parameter doesn't seems to be used yet
-        // see https://github.com/flatpak/xdg-desktop-portal/blob/1.20.0/src/camera.c#L273
-        let options: HashMap<&str, Value<'_>> = HashMap::new();
+    pub async fn open_pipe_wire_remote(
+        &self,
+        options: OpenPipeWireRemoteOptions,
+    ) -> Result<OwnedFd, Error> {
         let fd = self
             .0
             .call::<zvariant::OwnedFd>("OpenPipeWireRemote", &options)
@@ -271,9 +276,9 @@ pub async fn pipewire_streams(fd: OwnedFd) -> Result<Vec<Stream>, pipewire::Erro
 /// available.
 pub async fn request() -> Result<Option<OwnedFd>, Error> {
     let proxy = Camera::new().await?;
-    proxy.request_access().await?;
+    proxy.request_access(Default::default()).await?;
     if proxy.is_present().await? {
-        Ok(Some(proxy.open_pipe_wire_remote().await?))
+        Ok(Some(proxy.open_pipe_wire_remote(Default::default()).await?))
     } else {
         Ok(None)
     }
@@ -285,9 +290,9 @@ pub async fn request() -> Result<Option<OwnedFd>, Error> {
 /// available streams, one per camera.
 pub async fn request() -> Result<Option<(OwnedFd, Vec<Stream>)>, Error> {
     let proxy = Camera::new().await?;
-    proxy.request_access().await?;
+    proxy.request_access(Default::default()).await?;
     if proxy.is_present().await? {
-        let fd = proxy.open_pipe_wire_remote().await?;
+        let fd = proxy.open_pipe_wire_remote(Default::default()).await?;
         let streams = pipewire_streams(fd.try_clone()?).await?;
         Ok(Some((fd, streams)))
     } else {

client/src/desktop/dynamic_launcher.rs
@@ -6,7 +6,7 @@
 //! use std::io::Read;
 //! use ashpd::{
 //!     desktop::{
-//!         dynamic_launcher::{DynamicLauncherProxy, PrepareInstallOptions},
+//!         dynamic_launcher::DynamicLauncherProxy,
 //!         Icon,
 //!     },
 //!     WindowIdentifier,
@@ -27,7 +27,7 @@
 //!             None,
 //!             "SomeApp",
 //!             icon,
-//!             PrepareInstallOptions::default()
+//!             Default::default()
 //!         )
 //!         .await?
 //!         .response()?;
@@ -42,15 +42,13 @@
 //!         Type=Application
 //!     "#;
 //!     proxy
-//!         .install(&token, "some_file.desktop", desktop_entry)
+//!         .install(&token, "some_file.desktop", desktop_entry, Default::default())
 //!         .await?;
 //!
 //!     proxy.uninstall("some_file.desktop").await?;
 //!     Ok(())
 //! }
 //! ```
-
-use std::collections::HashMap;
 
 use enumflags2::{BitFlags, bitflags};
 use serde::{Deserialize, Serialize};
@@ -226,6 +224,22 @@ impl LaunchOptions {
     }
 }
 
+#[derive(Serialize, Type, Debug, Default)]
+/// Specified options for a [`DynamicLauncherProxy::install`] request.
+#[zvariant(signature = "dict")]
+pub struct InstallOptions {}
+
+#[derive(Serialize, Type, Debug, Default)]
+/// Specified options for a [`DynamicLauncherProxy::uninstall`] request.
+#[zvariant(signature = "dict")]
+pub struct UninstallOptions {}
+
+#[derive(Serialize, Type, Debug, Default)]
+/// Specified options for a [`DynamicLauncherProxy::request_install_token`]
+/// request.
+#[zvariant(signature = "dict")]
+pub struct RequestInstallTokenOptions {}
+
 #[derive(Debug)]
 /// Wrong type of [`crate::desktop::Icon`] was used.
 pub struct UnexpectedIconError;
@@ -302,13 +316,16 @@ impl DynamicLauncherProxy {
     /// See also [`RequestInstallToken`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.DynamicLauncher.html#org-freedesktop-portal-dynamiclauncher-requestinstalltoken).
     #[doc(alias = "RequestInstallToken")]
     #[doc(alias = "xdp_portal_dynamic_launcher_request_install_token")]
-    pub async fn request_install_token(&self, name: &str, icon: Icon) -> Result<String, Error> {
+    pub async fn request_install_token(
+        &self,
+        name: &str,
+        icon: Icon,
+        options: RequestInstallTokenOptions,
+    ) -> Result<String, Error> {
         if !icon.is_bytes() {
             return Err(UnexpectedIconError {}.into());
         }
 
-        // No supported options for now
-        let options: HashMap<&str, zvariant::Value<'_>> = HashMap::new();
         self.0
             .call::<String>("RequestInstallToken", &(name, icon.as_value(), options))
             .await
@@ -324,9 +341,8 @@ impl DynamicLauncherProxy {
         token: &str,
         desktop_file_id: &str,
         desktop_entry: &str,
+        options: InstallOptions,
     ) -> Result<(), Error> {
-        // No supported options for now
-        let options: HashMap<&str, zvariant::Value<'_>> = HashMap::new();
         self.0
             .call::<()>("Install", &(token, desktop_file_id, desktop_entry, options))
             .await
@@ -337,9 +353,11 @@ impl DynamicLauncherProxy {
     /// See also [`Uninstall`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.DynamicLauncher.html#org-freedesktop-portal-dynamiclauncher-uninstall).
     #[doc(alias = "Uninstall")]
     #[doc(alias = "xdp_portal_dynamic_launcher_uninstall")]
-    pub async fn uninstall(&self, desktop_file_id: &str) -> Result<(), Error> {
-        // No supported options for now
-        let options: HashMap<&str, zvariant::Value<'_>> = HashMap::new();
+    pub async fn uninstall(
+        &self,
+        desktop_file_id: &str,
+        options: UninstallOptions,
+    ) -> Result<(), Error> {
         self.0
             .call::<()>("Uninstall", &(desktop_file_id, options))
             .await

client/src/desktop/global_shortcuts.rs
@@ -89,10 +89,10 @@ impl Shortcut {
     }
 }
 
-/// Specified options for a [`GlobalShortcuts::create_session`] request.
 #[derive(Serialize, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
-struct CreateSessionOptions {
+/// Specified options for a [`GlobalShortcuts::create_session`] request.
+pub struct CreateSessionOptions {
     /// A string that will be used as the last element of the handle.
     #[serde(with = "as_value")]
     handle_token: HandleToken,
@@ -101,10 +101,10 @@ struct CreateSessionOptions {
     session_handle_token: HandleToken,
 }
 
-/// Specified options for a [`GlobalShortcuts::bind_shortcuts`] request.
 #[derive(Serialize, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
-struct BindShortcutsOptions {
+/// Specified options for a [`GlobalShortcuts::bind_shortcuts`] request.
+pub struct BindShortcutsOptions {
     /// A string that will be used as the last element of the handle.
     #[serde(with = "as_value")]
     handle_token: HandleToken,
@@ -127,15 +127,28 @@ impl BindShortcuts {
 
 #[derive(Serialize, Type, Debug)]
 #[zvariant(signature = "dict")]
-struct ConfigureShortcutsOptions {
+/// Specified options for a [`GlobalShortcuts::configure_shortcuts`] request.
+pub struct ConfigureShortcutsOptions {
     #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     activation_token: Option<ActivationToken>,
 }
 
-/// Specified options for a [`GlobalShortcuts::list_shortcuts`] request.
+impl ConfigureShortcutsOptions {
+    /// Sets the token that can be used to activate the configuration dialog.
+    #[must_use]
+    pub fn activation_token(
+        mut self,
+        activation_token: impl Into<Option<ActivationToken>>,
+    ) -> Self {
+        self.activation_token = activation_token.into();
+        self
+    }
+}
+
 #[derive(Serialize, Type, Debug, Default)]
 #[zvariant(signature = "dict")]
-struct ListShortcutsOptions {
+/// Specified options for a [`GlobalShortcuts::list_shortcuts`] request.
+pub struct ListShortcutsOptions {
     /// A string that will be used as the last element of the handle.
     #[serde(with = "as_value")]
     handle_token: HandleToken,
@@ -259,8 +272,10 @@ impl GlobalShortcuts {
     ///
     /// See also [`CreateSession`](https://flatpak.github.io/xdg-desktop-portal/docs/doc-org.freedesktop.portal.GlobalShortcuts.html#org-freedesktop-portal-globalshortcuts-createsession).
     #[doc(alias = "CreateSession")]
-    pub async fn create_session(&self) -> Result<Session<Self>, Error> {
-        let options = CreateSessionOptions::default();
+    pub async fn create_session(
+        &self,
+        options: CreateSessionOptions,
+    ) -> Result<Session<Self>, Error> {
         let (request, proxy) = futures_util::try_join!(
             self.0.request::<CreateSessionResponse>(
                 &options.handle_token,
@@ -284,8 +299,8 @@ impl GlobalShortcuts {
         session: &Session<Self>,
         shortcuts: &[NewShortcut],
         identifier: Option<&WindowIdentifier>,
+        options: BindShortcutsOptions,
     ) -> Result<Request<BindShortcuts>, Error> {
-        let options = BindShortcutsOptions::default();
         let identifier = Optional::from(identifier);
         self.0
             .request(
@@ -305,8 +320,8 @@ impl GlobalShortcuts {
     pub async fn list_shortcuts(
         &self,
         session: &Session<Self>,
+        options: ListShortcutsOptions,
     ) -> Result<Request<ListShortcuts>, Error> {
-        let options = ListShortcutsOptions::default();
         self.0
             .request(&options.handle_token, "ListShortcuts", &(session, &options))
             .await
@@ -323,11 +338,8 @@ impl GlobalShortcuts {
         &self,
         session: &Session<Self>,
         identifier: Option<&WindowIdentifier>,
-        activation_token: impl Into<Option<ActivationToken>>,
+        options: ConfigureShortcutsOptions,
     ) -> Result<(), Error> {
-        let options = ConfigureShortcutsOptions {
-            activation_token: activation_token.into(),
-        };
         let identifier = Optional::from(identifier);
 
         self.0

client/src/desktop/inhibit.rs
@@ -10,7 +10,7 @@
 //!
 //! async fn run() -> ashpd::Result<()> {
 //!     let proxy = InhibitProxy::new().await?;
-//!     let session = proxy.create_monitor(None).await?;
+//!     let session = proxy.create_monitor(None, Default::default()).await?;
 //!
 //!     let state = proxy.receive_state_changed().await?.next().await.unwrap();
 //!     match state.session_state() {
@@ -49,25 +49,30 @@ use crate::{Error, WindowIdentifier, desktop::session::CreateSessionResponse, pr
 #[derive(Serialize, Type, Debug, Default)]
 /// Specified options for a [`InhibitProxy::create_monitor`] request.
 #[zvariant(signature = "dict")]
-struct CreateMonitorOptions {
-    /// A string that will be used as the last element of the handle.
+pub struct CreateMonitorOptions {
     #[serde(with = "as_value")]
     handle_token: HandleToken,
-    /// A string that will be used as the last element of the session handle.
     #[serde(with = "as_value")]
     session_handle_token: HandleToken,
 }
 
 #[derive(Serialize, Type, Debug, Default)]
-/// Specified options for a [`InhibitProxy::inhibit`] request.
 #[zvariant(signature = "dict")]
-struct InhibitOptions {
-    /// A string that will be used as the last element of the handle.
+/// Specified options for a [`InhibitProxy::inhibit`] request.
+pub struct InhibitOptions {
     #[serde(with = "as_value")]
     handle_token: HandleToken,
-    /// User-visible reason for the inhibition.
     #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     reason: Option<String>,
+}
+
+impl InhibitOptions {
+    /// Sets a user-visible reason for the request.
+    #[must_use]
+    pub fn reason<'a>(mut self, reason: impl Into<Option<&'a str>>) -> Self {
+        self.reason = reason.into().map(ToOwned::to_owned);
+        self
+    }
 }
 
 #[bitflags]
@@ -183,8 +188,8 @@ impl InhibitProxy {
     pub async fn create_monitor(
         &self,
         identifier: Option<&WindowIdentifier>,
+        options: CreateMonitorOptions,
     ) -> Result<Session<Self>, Error> {
-        let options = CreateMonitorOptions::default();
         let identifier = Optional::from(identifier);
         let body = &(identifier, &options);
         let (monitor, proxy) = futures_util::try_join!(
@@ -202,7 +207,6 @@ impl InhibitProxy {
     ///
     /// * `identifier` - The application window identifier.
     /// * `flags` - The flags determine what changes are inhibited.
-    /// * `reason` - User-visible reason for the inhibition.
     ///
     /// # Specifications
     ///
@@ -213,12 +217,8 @@ impl InhibitProxy {
         &self,
         identifier: Option<&WindowIdentifier>,
         flags: BitFlags<InhibitFlags>,
-        reason: &str,
+        options: InhibitOptions,
     ) -> Result<Request<()>, Error> {
-        let options = InhibitOptions {
-            reason: Some(reason.to_owned()),
-            handle_token: Default::default(),
-        };
         let identifier = Optional::from(identifier);
         self.0
             .empty_request(

commit b495b35d157d0c1c7c9e3a22fa0033a0cf12699e

clipboard: Add missing mime types setter to SetSelectionOptions
Fixes: effaceb359ca ("Always use an Options struct")

client/src/desktop/clipboard.rs
@@ -19,6 +19,14 @@ pub struct SetSelectionOptions<'a> {
     mime_types: &'a [&'a str],
 }
 
+impl<'a> SetSelectionOptions<'a> {
+    /// Sets the mime types for the clipboard selection.
+    pub fn mime_types(mut self, mime_types: &'a [&'a str]) -> Self {
+        self.mime_types = mime_types;
+        self
+    }
+}
+
 #[derive(Debug, Type, Serialize, Default)]
 #[zvariant(signature = "dict")]
 /// Specified options for a [`Clipboard::request`] request.