bilelmoussaoui/ashpd

commit f1407ee0a79e51e0e8aa6c03942b3e34b99dbcf3

clipboard: Add missing mime types setter to SetSelectionOptions
Fixes: effaceb359ca ("Always use an Options struct")

client/src/desktop/clipboard.rs
@@ -19,6 +19,14 @@ pub struct SetSelectionOptions<'a> {
     mime_types: &'a [&'a str],
 }
 
+impl<'a> SetSelectionOptions<'a> {
+    /// Sets the mime types for the clipboard selection.
+    pub fn mime_types(mut self, mime_types: &'a [&'a str]) -> Self {
+        self.mime_types = mime_types;
+        self
+    }
+}
+
 #[derive(Debug, Type, Serialize, Default)]
 #[zvariant(signature = "dict")]
 /// Specified options for a [`Clipboard::request`] request.

commit df024f29a30bf8ed7b4234da7c2b4c6fa80a4b27

clipboard: Use set_mime_types in SetSelectionOptions
Convention is that setters in options are set_foo(), getters in options
are just foo().

Fixes: f1407ee0a79e ("clipboard: Add missing mime types setter to SetSelectionOptions")

client/src/desktop/clipboard.rs
@@ -25,7 +25,7 @@ pub struct SetSelectionOptions<'a> {
 
 impl<'a> SetSelectionOptions<'a> {
     /// Sets the mime types for the clipboard selection.
-    pub fn mime_types(mut self, mime_types: &'a [&'a str]) -> Self {
+    pub fn set_mime_types(mut self, mime_types: &'a [&'a str]) -> Self {
         self.mime_types = mime_types;
         self
     }