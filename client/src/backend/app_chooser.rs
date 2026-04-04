bilelmoussaoui/ashpd

commit 751f2096a8cc90399d4c7db9302ed3fd8dbc2bba

app_chooser: fix Choice to accept DesktopID
https://github.com/bilelmoussaoui/ashpd/issues/402

client/src/backend/app_chooser.rs
@@ -28,6 +28,15 @@ impl DesktopID {
     }
 }
 
+impl Serialize for DesktopID {
+    fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: serde::Serializer,
+    {
+        serializer.serialize_str(self.to_string().as_str())
+    }
+}
+
 impl<'de> Deserialize<'de> for DesktopID {
     fn deserialize<D>(deserializer: D) -> std::result::Result<Self, D::Error>
     where
@@ -94,13 +103,13 @@ impl ChooserOptions {
 #[zvariant(signature = "dict")]
 pub struct Choice {
     #[serde(with = "as_value")]
-    choice: AppID,
+    choice: DesktopID,
     #[serde(with = "optional", skip_serializing_if = "Option::is_none")]
     activation_token: Option<ActivationToken>,
 }
 
 impl Choice {
-    pub fn new(choice: AppID) -> Self {
+    pub fn new(choice: DesktopID) -> Self {
         Self {
             choice,
             activation_token: None,

commit 2f7a13ae7672be566e17c0aa509f6c2679aef9ab

app-chooser: Allow to build DesktopID from AppID
This makes it simple to construct a desktop-id from an app-id.

Fixes: 751f209 ("app_chooser: fix Choice to accept DesktopID")
Signed-off-by: Guido Günther <agx@sigxcpu.org>

client/src/backend/app_chooser.rs
@@ -28,6 +28,12 @@ impl DesktopID {
     }
 }
 
+impl From<AppID> for DesktopID {
+    fn from(value: AppID) -> Self {
+        Self(Ok(value))
+    }
+}
+
 impl Serialize for DesktopID {
     fn serialize<S>(&self, serializer: S) -> Result<S::Ok, S::Error>
     where